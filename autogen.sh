e92f31806657026e0eebd686fe8ef7362709560e
Tobias Rundstrom <tru@xmms.org>
2003-07-19 16:58:34 +0100

    Fix for running autogen in gtk2 client dir.
    BK KEY: tru@xmms.org|ChangeSet|20030719175834|19298

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+(cd src/clients/gtk2 ; ./autogen.sh)
+
 libtoolize --automake -c -f
 aclocal -I acmacros
 autoheader
--- src/clients/gtk2/autogen.sh
+++ src/clients/gtk2/autogen.sh
@@ -150,10 +150,3 @@ done
 
 conf_flags="--enable-maintainer-mode"
 
-if test x$NOCONFIGURE = x; then
-  echo Running $srcdir/configure $conf_flags "$@" ...
-  $srcdir/configure $conf_flags "$@" \
-  && echo Now type \`make\' to compile. || exit 1
-else
-  echo Skipping configure process.
-fi
