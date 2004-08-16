bba560971876f87de6f1f8f323ad832f4bc6ec58
Tilman Sauerbeck <tilman@code-monkey.de>
2004-08-16 19:08:00 +0100

    Large amount of fixes to the M3U plugin.
    BK KEY: tilman@code-monkey.de|ChangeSet|20040816200800|10560

--- src/plugins/m3u/m3u.c
+++ src/plugins/m3u/m3u.c
@@ -1,13 +1,13 @@
 /*  XMMS2 - X Music Multiplexer System
  *  Copyright (C) 2003	Peter Alm, Tobias Rundström, Anders Gustafsson
- * 
+ *
  *  PLUGINS ARE NOT CONSIDERED TO BE DERIVED WORK !!!
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- *                   
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -37,9 +37,9 @@
  */
 
 static gboolean xmms_m3u_can_handle (const gchar *mimetype);
-static gboolean xmms_m3u_read_playlist (xmms_transport_t *transport, 
+static gboolean xmms_m3u_read_playlist (xmms_transport_t *transport,
 		xmms_playlist_t *playlist);
-static gboolean xmms_m3u_write_playlist (xmms_playlist_t *playlist, 
+static gboolean xmms_m3u_write_playlist (xmms_playlist_t *playlist,
 		gchar *filename);
 
 /*
@@ -57,12 +57,12 @@ xmms_plugin_get (void)
 
 	xmms_plugin_info_add (plugin, "URL", "http://www.xmms.org/");
 	xmms_plugin_info_add (plugin, "Author", "XMMS Team");
-	
+
 	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_CAN_HANDLE,
 				xmms_m3u_can_handle);
-	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_READ_PLAYLIST, 
+	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_READ_PLAYLIST,
 				xmms_m3u_read_playlist);
-	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_WRITE_PLAYLIST, 
+	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_WRITE_PLAYLIST,
 				xmms_m3u_write_playlist);
 
 	return plugin;
@@ -78,7 +78,7 @@ xmms_m3u_can_handle (const gchar *mime)
 	g_return_val_if_fail (mime, FALSE);
 
 	XMMS_DBG ("xmms_m3u_can_handle (%s)", mime);
-	
+
 	if ((g_strncasecmp (mime, "audio/mpegurl", 13) == 0))
 		return TRUE;
 
@@ -88,204 +88,218 @@ xmms_m3u_can_handle (const gchar *mime)
 	return FALSE;
 }
 
-static gboolean
-xmms_m3u_read_playlist (xmms_transport_t *transport, 
-			xmms_playlist_t *playlist)
+static xmms_playlist_entry_t *
+parse_line (const gchar *line, const gchar *m3u_path)
 {
-	gint len = 0;
-	gint buffer_len = 0;
-	gint i;
-	gchar *buffer;
-	gchar **lines;
+	xmms_playlist_entry_t *entry = NULL;
+	gchar newp[XMMS_MAX_URI_LEN + 1], *encoded = NULL, *p;
+
+	g_assert (line);
+
+	/* used to determine whether we got a valid path/url, see below */
+	newp[0] = '\0';
+
+	/* check for an absolute path */
+	if (line[0] == '/') {
+		encoded = xmms_util_encode_path (line);
+		g_snprintf (newp, sizeof (newp), "file://%s", encoded);
+	} else {
+		/* check whether it's an url (proto://...) */
+		p = strchr (line, ':');
+
+		if (p && p[1] == '/' && p[2] == '/') {
+			/* FIXME: how do we know whether the url is properly
+			 * encoded?
+			 * the problem is, if we encode an encoded path, all we'll
+			 * get is junk :/
+			 */
+			encoded = xmms_util_encode_path (line);
+			g_snprintf (newp, sizeof (newp), "%s", encoded);
+		} else {
+			/* get the directory from the path to the m3u file.
+			 * since we must not mess with the original path, get our
+			 * own copy now:
+			 */
+			m3u_path = g_strdup (m3u_path);
+
+			p = strrchr (m3u_path, '/');
+			g_assert (p); /* m3u_path is always an encoded path */
+			*p = '\0';
+
+			encoded = xmms_util_decode_path (line);
+			g_snprintf (newp, sizeof (newp), "%s/%s",
+			            m3u_path, encoded);
+			g_free ((gchar *) m3u_path); /* free our copy */
+		}
+	}
+
+	/* in all code paths, newp should have been written */
+	g_assert (newp[0]);
+
+	entry = xmms_playlist_entry_new (newp);
+	g_free (encoded);
+
+	return entry;
+}
+
+static gboolean
+xmms_m3u_read_playlist (xmms_transport_t *transport,
+                        xmms_playlist_t *playlist)
+{
+	gint len = 0, buffer_len = 0;
+	gchar **lines, **line, *buffer;
 	gboolean extm3u = FALSE;
 
 	g_return_val_if_fail (transport, FALSE);
 	g_return_val_if_fail (playlist, FALSE);
-	
+
 	buffer_len = xmms_transport_size (transport);
 	buffer = g_malloc0 (buffer_len);
 
 	while (len < buffer_len) {
 		gint ret;
 
-		ret = xmms_transport_read (transport, buffer+len, buffer_len);
+		ret = xmms_transport_read (transport, buffer + len, buffer_len);
 		XMMS_DBG ("Got %d bytes.", ret);
 
-		if ( ret < 0 ) {
+		if (ret < 0) {
 			g_free (buffer);
-			buffer=NULL;
 			return FALSE;
 		}
+
 		len += ret;
 		g_assert (len >= 0);
 	}
 
 	lines = g_strsplit (buffer, "\n", 0);
+	g_free (buffer);
 
-	if (!lines && !lines[0])
+	if (!lines)
 		return FALSE;
 
-	i = 0;
-
-	if (strcmp (lines[0], "#EXTM3U") == 0) {
-		extm3u = TRUE;
-		i = 1;
+	line = lines;
+	if (!line) {
+		g_strfreev (lines);
+		return FALSE;
 	}
 
-	while (lines[i]) {
-		xmms_playlist_entry_t *entry = NULL;
-		gchar *t;
+	if (strcmp (*line, "#EXTM3U") == 0) {
+		extm3u = TRUE;
+		line++;
+	}
 
-		if (extm3u && lines[i][0] == '#') {
-			gchar *len;
-			gchar *title;
-			gchar *p;
+	while (*line) {
+		xmms_playlist_entry_t *entry = NULL;
+
+		if (!**line) {
+			line++;
+			continue;
+		}
+
+		if (extm3u && **line == '#') {
+			gchar *title, *p;
 			int read, write;
 
-			p = strchr (lines[i], ',');
+			p = strchr (*line, ',');
 			if (p) {
 				*p = '\0';
 				*p++;
 			} else {
 				xmms_log_error ("Malformated m3u");
+				g_strfreev (lines);
 				return FALSE;
 			}
 
-			len = lines[i]+8;
-			title = g_convert (p, strlen (p), "UTF-8", "ISO-8859-1", &read, &write, NULL);
+			/* FIXME: check whether the data we read is actually
+			 * ISO-8859-1, might be anything else as well.
+			 */
+			title = g_convert (p, strlen (p), "UTF-8", "ISO-8859-1",
+			                   &read, &write, NULL);
 
-			i++; /* skip to track */
+			line++; /* skip to track */
 
-			if (lines[i] && lines[i][0]) {
-				if (lines[i][0] != '/') {
-					gchar *new, *path, *p;
-					path = g_strdup (xmms_transport_url_get (transport));
-					
-					p = strrchr (path, '/');
-					if (p) {
-						*p = '\0';
-						new = g_strdup_printf ("%s/%s", path, lines[i]);
-						t = xmms_util_encode_path (new);
-						entry = xmms_playlist_entry_new (t);
-						g_free (new);
-						g_free (path);
-					}
-				} else {
-					gchar *new;
-					new = g_strdup_printf ("file://%s", lines[i]);
-					t = xmms_util_encode_path (new);
-					entry = xmms_playlist_entry_new (t);
-					g_free (new);
-				}
+			if (*line) {
+				entry = parse_line (*line,
+				                    xmms_transport_url_get (transport));
 			}
 
-			xmms_playlist_entry_property_set (entry, XMMS_PLAYLIST_ENTRY_PROPERTY_DURATION, len);
+			g_assert (entry);
+
+			xmms_playlist_entry_property_set (entry, XMMS_PLAYLIST_ENTRY_PROPERTY_DURATION,
+			                                  *line + 8);
 			xmms_playlist_entry_property_set (entry, XMMS_PLAYLIST_ENTRY_PROPERTY_TITLE, title);
+			g_free (title);
 		} else {
-			if (lines[i] && lines[i][0]) {
-				if (lines[i][0] != '/') {
-					gchar *new, *path, *p;
-					path = g_strdup (xmms_transport_url_get (transport));
-					
-					p = strrchr (path, '/');
-					if (p) {
-						*p = '\0';
-						new = g_strdup_printf ("%s/%s", path, lines[i]);
-						t = xmms_util_encode_path (new);
-						entry = xmms_playlist_entry_new (t);
-						g_free (new);
-						g_free (path);
-					}
-				} else {
-					gchar *new;
-					new = g_strdup_printf ("file://%s", lines[i]);
-					t = xmms_util_encode_path (new);
-					entry = xmms_playlist_entry_new (lines[i]);
-					g_free (new);
-				}
-			}
+			entry = parse_line (*line,
+			                    xmms_transport_url_get (transport));
 		}
 
-		if (entry) {
-			XMMS_DBG ("Adding %s", xmms_playlist_entry_url_get (entry));
-			xmms_playlist_add (playlist, entry, XMMS_PLAYLIST_APPEND);
-		}
-			
-		i++;
+		g_assert (entry);
+
+		XMMS_DBG ("Adding %s", xmms_playlist_entry_url_get (entry));
+		xmms_playlist_add (playlist, entry, XMMS_PLAYLIST_APPEND);
+
+		if (*line)
+			line++;
 	}
 
-	return TRUE;
+	g_strfreev (lines);
 
+	return TRUE;
 }
 
 static gboolean
 xmms_m3u_write_playlist (xmms_playlist_t *playlist, gchar *filename)
 {
 	FILE *fp;
-	GList *lista, *tmp;
+	GList *list, *l;
 	xmms_error_t err;
+
+	g_return_val_if_fail (playlist, FALSE);
+	g_return_val_if_fail (filename, FALSE);
 
 	xmms_error_reset (&err);
 
-	fp = fopen (filename, "w+");
+	fp = fopen (filename, "w");
 	if (!fp)
 		return FALSE;
 
-	lista = xmms_playlist_list (playlist, &err);
+	list = xmms_playlist_list (playlist, &err);
 
-	fwrite ("#EXTM3U\n", 8, 1, fp);
+	fprintf (fp, "#EXTM3U\n");
 
-	for (tmp = lista; tmp; tmp = g_list_next (tmp)) {
-		xmms_playlist_entry_t *entry;
-		gchar *d, *artist, *title;
-		gint duration;
+	for (l = list; l; l = l->next) {
+		xmms_playlist_entry_t *entry = l->data;
 		gchar *url;
-		gchar *url2;
+		const gchar *d, *artist, *title;
+		gint duration = 0;
 
-		entry = (xmms_playlist_entry_t *)tmp->data;
-
-		d = xmms_playlist_entry_property_get (entry, 
+		d = xmms_playlist_entry_property_get (entry,
 				XMMS_PLAYLIST_ENTRY_PROPERTY_DURATION);
-
 		if (d)
 			duration = atoi (d);
-		else
-			d = 0;
 
 		artist = xmms_playlist_entry_property_get (entry,
 				XMMS_PLAYLIST_ENTRY_PROPERTY_ARTIST);
-		
+
 		title = xmms_playlist_entry_property_get (entry,
 				XMMS_PLAYLIST_ENTRY_PROPERTY_TITLE);
 
 		xmms_object_unref (entry);
 
 		if (title && artist && duration) {
-			gchar *t;
-
-			t = g_strdup_printf ("#EXTINF:%d,%s - %s\n", duration / 1000,
-					artist, title);
-
-			fwrite (t, strlen (t), 1, fp);
-			g_free (t);
+			fprintf (fp, "#EXTINF:%d,%s - %s\n", duration / 1000,
+			         artist, title);
 		}
 
-		url2 = xmms_util_decode_path (xmms_playlist_entry_url_get (entry));
-		url = strchr (url2, ':');
-		if (!url)
-			continue;
+		url = xmms_util_decode_path (xmms_playlist_entry_url_get (entry));
+		g_assert (url);
 
-		url = url + 2;
-
-		url = g_strdup_printf ("%s\n", url);
-
-		fwrite (url, strlen (url), 1, fp);
-
+		fprintf (fp, "%s\n", url);
 		g_free (url);
-		g_free (url2);
 	}
-	
+
 	fclose (fp);
-	
+
 	return TRUE;
 }
