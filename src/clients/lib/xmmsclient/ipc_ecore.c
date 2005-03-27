c2f1e1c97a448fb9e3d861594c06a11130e0b22f
Tilman Sauerbeck <tilman@code-monkey.de>
2005-03-27 20:11:16 +0100

    The Ecore IPC code now also checks for writable state of the file descriptor.
    BK KEY: tilman@code-monkey.de|ChangeSet|20050327211116|49907

--- src/clients/lib/xmmsclient/ipc_ecore.c
+++ src/clients/lib/xmmsclient/ipc_ecore.c
@@ -15,18 +15,25 @@ on_fd_data (void *udata, Ecore_Fd_Handler *handler)
 	if (ecore_main_fd_handler_active_get (handler, ECORE_FD_ERROR)) {
 		xmmsc_ipc_error_set (ipc, "Remote host disconnected, or something!");
 		xmmsc_ipc_disconnect (ipc);
-	} else if (ecore_main_fd_handler_active_get (handler, ECORE_FD_READ)) {
-		ret = xmmsc_ipc_io_in_callback (ipc);
+
+		return 0;
 	}
 
-	return ret;
+	if (ecore_main_fd_handler_active_get (handler, ECORE_FD_READ))
+		xmmsc_ipc_io_in_callback (ipc);
+
+	if (ecore_main_fd_handler_active_get (handler, ECORE_FD_WRITE))
+		xmmsc_ipc_io_out_callback (ipc);
+
+	return 1;
 }
 
 gboolean
 xmmsc_ipc_setup_with_ecore (xmmsc_connection_t *c)
 {
 	ecore_main_fd_handler_add (xmmsc_ipc_fd_get (c->ipc),
-	                           ECORE_FD_READ | ECORE_FD_ERROR,
+	                           ECORE_FD_READ | ECORE_FD_WRITE |
+	                           ECORE_FD_ERROR,
 	                           on_fd_data, c->ipc, NULL, NULL);
 
 	return TRUE;
