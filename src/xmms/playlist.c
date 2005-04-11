e77458ed10d45a403fe2eb11f0b7cef0236d4972
Tobias Rundstrom <tru@xmms.org>
2005-04-11 17:29:46 +0100

    update currentpos correctly when you move entries around in the playlist.
    BK KEY: tru@xmms.org|ChangeSet|20050411182946|26276

--- src/xmms/playlist.c
+++ src/xmms/playlist.c
@@ -382,7 +382,11 @@ xmms_playlist_move (xmms_playlist_t *playlist, guint pos, gint newpos, xmms_erro
 	g_array_remove_index (playlist->list, pos);
 	g_array_insert_val (playlist->list, newpos, id);
 
-	if (pos == playlist->currentpos)
+	if (newpos <= playlist->currentpos && pos > playlist->currentpos)
+		playlist->currentpos++;
+	else if (newpos >= playlist->currentpos && pos < playlist->currentpos)
+		playlist->currentpos--;
+	else if (pos == playlist->currentpos)
 		playlist->currentpos = newpos;
 
 	XMMS_PLAYLIST_CHANGED_MSG (XMMS_PLAYLIST_CHANGED_MOVE, pos, newpos);
