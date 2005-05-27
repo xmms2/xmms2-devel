10414f4d4c7b0109cd97bde4c1f02757c0806304
Tilman Sauerbeck <tilman@code-monkey.de>
2005-05-27 17:19:59 +0200

    Fixed bug 177.

--- src/xmms/sqlite.c
+++ src/xmms/sqlite.c
@@ -30,16 +30,17 @@
 #include <glib.h>
 
 /* increment this whenever there are incompatible db structure changes */
-#define DB_VERSION 12
+#define DB_VERSION 13
 
 const char create_Control_stm[] = "create table Control (version)";
 const char create_Media_stm[] = "create table Media (id integer primary_key, key, value)";
 const char create_Log_stm[] = "create table Log (id, starttime, value)";
 const char create_Playlist_stm[] = "create table Playlist (id primary key, name)";
-const char create_PlaylistEntries_stm[] = "create table PlaylistEntries (playlist_id, entry, pos int, primary key (playlist_id, entry))";
+const char create_PlaylistEntries_stm[] = "create table PlaylistEntries (playlist_id int, entry, pos int)";
 const char create_idx_stm[] = "create unique index key_idx on Media (id, key);"
 			      "create index prop_idx on Media (key,value);"
                               "create index log_id on Log (id);"
+                              "create index playlistentries_idx on PlaylistEntries (playlist_id, entry);"
                               "create index playlist_idx on Playlist (name);";
 
 const char create_views[] = "CREATE VIEW artists as select distinct m1.value as artist from Media m1 left join Media m2 on m1.id = m2.id and m2.key='compilation' where m1.key='artist' and m2.value is null;"
@@ -98,8 +99,6 @@ xmms_sqlite_integer_coll (void *udata, int len1, const void *str1, int len2, con
 void
 upgrade_v11_to_v12 (sqlite3 *sql)
 {
-	gchar buf[128];
-
 	XMMS_DBG ("Preforming upgrade v11 to v12");
 	sqlite3_exec (sql, "drop view songs", NULL, NULL, NULL);
 	sqlite3_exec (sql, "drop view artists", NULL, NULL, NULL);
@@ -109,13 +108,48 @@ upgrade_v11_to_v12 (sqlite3 *sql)
 	sqlite3_exec (sql, create_views, NULL, NULL, NULL);
 	sqlite3_exec (sql, "delete from Control", NULL, NULL, NULL);
 
-	g_snprintf (buf, sizeof (buf),
-		    "insert into Control (version) VALUES (%i)",
-		    DB_VERSION);
+	XMMS_DBG ("done");
+}
 
-	sqlite3_exec (sql, buf, NULL, NULL, NULL);
+static void
+upgrade_v12_to_v13 (sqlite3 *sql)
+{
+	XMMS_DBG ("Preforming upgrade v12 to v13");
+
+	sqlite3_exec (sql, "drop table PlaylistEntries", NULL, NULL, NULL);
+	sqlite3_exec (sql, create_PlaylistEntries_stm, NULL, NULL, NULL);
 
 	XMMS_DBG ("done");
+}
+
+static gboolean
+try_upgrade (sqlite3 *sql, gint version)
+{
+	gchar buf[128];
+	gboolean can_upgrade = TRUE;
+
+	switch (version) {
+		case 11:
+			upgrade_v11_to_v12 (sql);
+			upgrade_v12_to_v13 (sql);
+			break;
+		case 12:
+			upgrade_v12_to_v13 (sql);
+			break;
+		default:
+			can_upgrade = FALSE;
+			break;
+	}
+
+	if (can_upgrade) {
+		/* store the new version in the database */
+		g_snprintf (buf, sizeof (buf),
+		            "insert into Control (version) VALUES (%i)",
+		            DB_VERSION);
+		sqlite3_exec (sql, buf, NULL, NULL, NULL);
+	}
+
+	return can_upgrade;
 }
 
 /**
@@ -153,23 +187,17 @@ xmms_sqlite_open (guint *id, gboolean *c)
 	if (!create) {
 		sqlite3_exec (sql, "select version from Control",
 			      xmms_sqlite_version_cb, &version, NULL);
-		if (version != DB_VERSION) {
+		if (version != DB_VERSION && !try_upgrade (sql, version)) {
 			gchar old[XMMS_PATH_MAX];
-			
-			if (version == 11) {
-				upgrade_v11_to_v12 (sql);
-			} else {
 
-				sqlite3_close (sql);
-				g_snprintf (old, XMMS_PATH_MAX, "%s/.xmms2/medialib.db.old", g_get_home_dir ());
-				rename (dbpath, old);
-	
-				if (sqlite3_open (dbpath, &sql)) {
-					xmms_log_fatal ("Error creating sqlite db: %s", sqlite3_errmsg (sql));
-					return NULL;
-				}
-
-				create = TRUE;
+			sqlite3_close (sql);
+			g_snprintf (old, XMMS_PATH_MAX, "%s/.xmms2/medialib.db.old",
+			            g_get_home_dir ());
+			rename (dbpath, old);
+			if (sqlite3_open (dbpath, &sql)) {
+				xmms_log_fatal ("Error creating sqlite db: %s",
+				                sqlite3_errmsg (sql));
+				return NULL;
 			}
 		}
 	}
