775554fff577471f056a7364d02a0cf365f1296a
Daniel Pearson <daniel@nanoo.org>
2005-06-13 20:28:33 +0200

    PYTHON: Added syncronous proxy class for API

--- src/clients/lib/python/xmmsclient.pyx
+++ src/clients/lib/python/xmmsclient.pyx
@@ -1335,3 +1335,57 @@ cdef class XMMS :
 		ret.more_init ()
 		return ret
 
+
+
+class XMMSSync:
+	"""
+	A wrapper for the xmmsclient.XMMS class which simplifies synchronous
+	communication with the XMMS2 daemon.
+	
+	Instances of this class may be used just like regular xmmsclient.XMMS
+	objects, except that instead of returning an XMMSResult instance, the
+	value associated with the result is returned.  If the XMMSResult
+	indicates an error, an XMMSError is raised instead of returning the
+	value.
+	"""
+	# This implementation avoids using nested function definitions, as they
+	# are not supported by Pyrex.
+	def __init__(self, xmms):
+		"""
+		This constructor takes a single argument which specifies the
+		XMMS object to wrap.
+		"""
+		self.__xmms = xmms
+
+	def __getattr__(self, name):
+		attr = getattr(self.__xmms, name)
+		if callable(attr):
+			return XMMSSyncMethod(attr)
+		else:
+			return attr
+
+
+class XMMSSyncMethod:
+	"""
+	A factory which uses a bound XMMS object method to create a callable
+	object that wraps the XMMS method for more convenient synchronous use.
+	This is meant for use by the XMMSSync class.
+	"""
+	def __init__(self, method):
+		self.method = method
+
+	def __call__(self, *args):
+		ret = self.method(*args)
+		if isinstance(ret, XMMSResult):
+			ret.wait()
+			if ret.iserror():
+				raise XMMSError(ret.get_error())
+			return ret.value()
+		return ret
+
+
+class XMMSError(Exception):
+	"""
+	Thrown when a synchronous method call on an XMMS client object fails.
+	"""
+	pass
