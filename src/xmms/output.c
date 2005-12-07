eab5b7bdc01767876d39ba513d132d0cdc0f2706
Anders Gustafsson <andersg@xmms.org>
2005-12-07 12:53:47 +0100

    BUG(478): Fix got lost in some merge.

--- src/xmms/output.c
+++ src/xmms/output.c
@@ -78,7 +78,6 @@ XMMS_CMD_DEFINE (currentid, xmms_output_current_id, xmms_output_t *, UINT32, NON
  *                playtime_mutex is leaflock.
  */
 
-
 struct xmms_output_St {
 	xmms_object_t object;
 	xmms_plugin_t *plugin;
@@ -919,9 +918,8 @@ status_changed (xmms_output_t *output, xmms_playback_status_t status)
 			g_return_if_fail (!output->write_running);
 			output->write_running = TRUE;
 
-			output->write_thread = g_thread_create (xmms_output_write_thread, output, TRUE, NULL);
+			output->write_thread = g_thread_create (xmms_output_write_thread, output, FALSE, NULL);
 		}
-		g_mutex_unlock (output->write_mutex);
 	} else if (status == XMMS_PLAYBACK_STATUS_STOP) {
 		XMMS_DBG ("Stopping playback!");
 
@@ -929,17 +927,13 @@ status_changed (xmms_output_t *output, xmms_playback_status_t status)
 		output->write_paused = FALSE;
 		g_cond_signal (output->write_cond);
 
-		g_mutex_unlock (output->write_mutex);
-
-		g_thread_join (output->write_thread);
-
 		output->write_thread = NULL;
 	} else if (status == XMMS_PLAYBACK_STATUS_PAUSE) {
 		XMMS_DBG ("Pausing playback!");
 		output->write_paused = TRUE;
-		g_mutex_unlock (output->write_mutex);
 	}
 
+	g_mutex_unlock (output->write_mutex);
 }
 
 static gboolean
