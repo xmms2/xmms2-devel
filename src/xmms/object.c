3468d0e1ddd84aba630293de1bb2660e18452565
Tilman Sauerbeck <tilman@code-monkey.de>
2005-12-30 05:09:29 +0100

    OTHER:Code cleanup.

--- src/xmms/object.c
+++ src/xmms/object.c
@@ -294,14 +294,12 @@ xmms_object_cmd_value_free (gpointer val)
 			break;
 		case XMMS_OBJECT_CMD_ARG_LIST:
 		case XMMS_OBJECT_CMD_ARG_PROPDICT:
-			{
-				GList *n, *nxt;
-				for (n = v->value.list; n; n = nxt) {
-					xmms_object_cmd_value_free (n->data);
-					nxt = g_list_next (n);
-					g_list_free_1 (n);
-				}
+			while (v->value.list) {
+				xmms_object_cmd_value_free (v->value.list->data);
+				v->value.list = g_list_delete_link (v->value.list,
+				                                    v->value.list);
 			}
+
 			break;
 		case XMMS_OBJECT_CMD_ARG_DICT:
 			g_hash_table_destroy (v->value.dict);
