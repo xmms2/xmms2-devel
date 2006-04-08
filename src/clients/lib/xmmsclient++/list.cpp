4fc455d62f9f69694ed667f5712f5a4a9952724a
Sebastien Cevey <seb@cine7.net>
2006-04-08 22:43:04 +0200

    OTHER: Remove deprecated Detail::*_foreach functions.

--- src/clients/lib/xmmsclient++/list.cpp
+++ src/clients/lib/xmmsclient++/list.cpp
@@ -68,43 +68,4 @@ namespace Xmms
 		return xmmsc_result_list_valid( result_ );
 	}
 
-	void Detail::dict_foreach( const void* key,
-	                           xmmsc_result_value_type_t type,
-							   const void* value,
-							   void* udata )
-	{
-
-		Dict* dict( static_cast< Dict* >( udata ) );
-		boost::any temp;
-		switch( type ) {
-			case XMMSC_RESULT_VALUE_TYPE_STRING: {
-				temp = std::string( static_cast< const char* >( value ) );
-				break;
-			}
-			case XMMSC_RESULT_VALUE_TYPE_UINT32: {
-				temp = reinterpret_cast< const uint32_t >( value );
-				break;
-			}
-			case XMMSC_RESULT_VALUE_TYPE_INT32: {
-				temp = reinterpret_cast< const int32_t >( value );
-				break;
-			}
-			case XMMSC_RESULT_VALUE_TYPE_NONE: {
-				// FIXME: probably not the right thing to do
-				return;
-			}
-		}
-
-		(*dict)[ static_cast< const char* >( key ) ] = temp;
-
-	}
-
-	void Detail::propdict_foreach( const void* key,
-	                               xmmsc_result_value_type_t type,
-								   const void* value,
-								   const char* source,
-								   void* udata )
-	{
-	}
-
 }
