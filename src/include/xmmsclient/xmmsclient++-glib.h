abed3fcf85a447c529d7543232fb852f018e6f40
Jonne Lehtinen <jonne.lehtinen@tut.fi>
2006-05-05 19:33:48 +0300

    OTHER: Comment GMainloop class.

--- src/include/xmmsclient/xmmsclient++-glib.h
+++ src/include/xmmsclient/xmmsclient++-glib.h
@@ -6,18 +6,32 @@
 namespace Xmms
 {
 
+	/** @class GMainloop xmmsclient++-glib.h "xmmsclient/xmmsclient++-glib.h"
+	 *  @brief Used to connect xmms2 ipc to glib (and gtkmm) mainloop.
+	 */
 	class GMainloop : public MainloopInterface
 	{
 
 		public:
 
+			/** Constructor, get the connection from Client class.
+			 *  @param conn Connection to bind to the glib mainloop.
+			 */
 			GMainloop( xmmsc_connection_t* conn );
+
+			/** Destructor.
+			 *  Disconnects from glib mainloop.
+			 */
 			~GMainloop();
 
+			/** Doesn't do anything, use glib mainloop functions instead.
+			 */
 			void run();
 
+		/** @cond */
 		private:
 			void* data;
+		/** @endcond */
 
 	};
 
