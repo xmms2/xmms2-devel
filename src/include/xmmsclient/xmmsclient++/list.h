2f2a169277293e26639f54e58ddefa3e99c5cfe2
Jonne Lehtinen <jonne.lehtinen@tut.fi>
2006-05-05 14:13:36 +0300

    OTHER: Comment List class (ignore the specializations from the doxygen, commenting on List<T> and SuperList should suffice).

--- src/include/xmmsclient/xmmsclient++/list.h
+++ src/include/xmmsclient/xmmsclient++/list.h
@@ -9,47 +9,108 @@
 #include <xmmsclient/xmmsclient++/exceptions.h>
 #include <string>
 
+#include <iostream>
+
 namespace Xmms
 {
 
+	/** @class SuperList list.h "xmmsclient/xmmsclient++/list.h"
+	 *  @brief Superclass for List classes.
+	 */
 	class SuperList
 	{
 
 		public:
+
+			/** Constructor.
+			 *
+			 *  @param result xmmsc_result_t* returned by one of the
+			 *         libxmmsclient functions, must be a list.
+			 *
+			 *  @throw result_error If the result was in error state.
+			 *  @throw not_list_error If the result is not a list.
+			 *
+			 *  @note You must unref the result you feed to this class.
+			 */
 			SuperList( xmmsc_result_t* result );
+
+			/** Copy-constructor.
+			 */
 			SuperList( const SuperList& list );
+
+			/** Copy assignment operator.
+			 */
 			virtual SuperList& operator=( const SuperList& list );
+
+			/** Destructor.
+			 */
 			virtual ~SuperList();
 
+			/** Return to first entry in list.
+			 *
+			 *  @todo Should probably throw on error?
+			 */
 			virtual void first();
+
+			/** Skip to next entry in list.
+			 *
+			 *  Advances to next list entry. May advance outside of list,
+			 *  so isValid should be used to determine if end of list was
+			 *  reached.
+			 *
+			 *  @todo Throw on error?
+			 */
 			virtual void operator++();
 
+			/** Check if current listnode is inside list boundary.
+			 */
 			virtual bool isValid() const;
 
 		protected:
 			xmmsc_result_t* result_;
 			bool constructed_;
 
+			/** This function should fetch the data from the listnode
+			 *  and set constructed_ to true.
+			 *  It's also a good idea to cache that data.
+			 */
 			virtual void constructContents() = 0;
 
 	};
 
-
+	/** @class List list.h "xmmsclient/xmmsclient++/list.h"
+	 *  @brief This class acts as a wrapper for list type results.
+	 *  This is actually a virtual class and is specialized with T being
+	 *  - std::string
+	 *  - int
+	 *  - unsigned int
+	 *  - Dict
+	 *
+	 *  If any other type is used, a compile-time error should occur.
+	 */
 	template< typename T >
 	class List : public SuperList
 	{
 
 		public:
+
+			/** Constructor
+			 *  @see SuperList#SuperList.
+			 */
 			List( xmmsc_result_t* result ) :
 				SuperList( result ), contents_()
 			{
 			}
 
+			/** Copy-constructor.
+			 */
 			List( const List<T>& list ) :
 				SuperList( list ), contents_( list.contents_ )
 			{
 			}
 
+			/** Copy assignment operator.
+			 */
 			List<T>& operator=( const List<T>& list )
 			{
 				SuperList::operator=( list );
@@ -57,29 +118,42 @@ namespace Xmms
 				return *this;
 			}
 
+			/** Destructor.
+			 */
 			virtual ~List()
 			{
 			}
 
+			/** Operator *.
+			 *  Used to get the underlying value from the list.
+			 */
 			const T& operator*()
 			{
 				constructContents();
 				return contents_;
 			}
 
+			/** Operator ->.
+			 *  Used to call a function of the underlying class
+			 *  (only applicable for std::string and Dict).
+			 *  Same as (*list).function();
+			 */
 			const T& operator->()
 			{
 				constructContents();
 				return contents_;
 			}
 			
+		/** @cond */
 		private:
 			T contents_;
 
 			virtual void constructContents() = 0;
+		/** @endcond */
 
 	};
 
+	/** @cond */
 	template<>
 	class List< int > : public SuperList
 	{
@@ -343,6 +417,7 @@ namespace Xmms
 			}
 
 	};
+	/** @endcond */
 }
 
 #endif // XMMSCLIENTPP_LIST_H
