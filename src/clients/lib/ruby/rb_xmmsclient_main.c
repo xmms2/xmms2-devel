8903e5eb06d6690f8c1d59a2800fa438a57c630d
Tilman Sauerbeck <tilman@code-monkey.de>
2006-05-20 14:35:48 +0200

    FEATURE(834): Added Xmms::VERSION.

--- src/clients/lib/ruby/rb_xmmsclient_main.c
+++ src/clients/lib/ruby/rb_xmmsclient_main.c
@@ -14,6 +14,8 @@
  *  Lesser General Public License for more details.
  */
 
+#include <xmms/xmms_defs.h>
+
 #include <ruby.h>
 
 void Init_Client ();
@@ -21,6 +23,8 @@ void Init_Client ();
 void Init_xmmsclient (void)
 {
 	VALUE mXmms = rb_define_module ("Xmms");
+
+	rb_define_const (mXmms, "VERSION", rb_str_new2 (XMMS_VERSION));
 
 	Init_Client (mXmms); /* initializes Result, too */
 }
