1983fbce4d11f30d5fb449ee5cc2b856e2aaaa56
Cole Thompson <cole@vector.(none)>
2006-07-14 19:33:09 -0500

    Removed standalone client (xform mostly complete now)

--- daap-test/daap_util.h
+++
@@ -1,8 +0,0 @@
-#ifndef DAAP_UTIL_H
-#define DAAP_UTIL_H
-
-gint read_buffer_from_channel(GIOChannel *chan, gchar *buf, gint bufsize);
-void write_buffer_to_channel(GIOChannel *chan, gchar *buf, gint bufsize);
-
-#endif
-/* vim:noexpandtab:shiftwidth=4:set tabstop=4: */
