4b6ab8c3aafcaae7529424843ac8adaa45bcebcb
David Anderson <danderson@naos.corp.google.com>
2006-11-28 00:47:28 -0800

    Refactor all plugins.

--- xmmsenv/plugin.py
+++ xmmsenv/plugin.py
@@ -2,18 +2,33 @@
 # common tasks carried out by plugins in order to configure and build
 # themselves.
 
-def plugin(name, sources=None, configure=False, build=False, build_replace=False):
+def plugin(name, source=None, configure=False, build=False,
+           build_replace=False, needs_lib=False, extra_libs=[],
+           tool='cc', broken=False):
   def stock_configure(conf):
+    if broken:
+      conf.check_message_custom('%s plugin' % name, '',
+                                'currently broken, disabled')
+      return
     if configure and not configure(conf):
       return
     conf.env['XMMS_PLUGINS_ENABLED'].append(name)
 
   def stock_build(bld):
-    obj = bld.create_obj('cc', 'plugin')
+    obj = bld.create_obj(tool, 'plugin')
     obj.target = 'xmms_%s' % name
     obj.includes = '../../include'
-    obj.source = '%s.c' % name
-    obj.uselib = 'glib-2.0'
+    if source:
+      obj.source = source
+    else:
+      obj.source = '%s.c' % name
+
+    libs = ['glib-2.0']
+    if needs_lib:
+      libs.append(name)
+    libs += extra_libs
+    obj.uselib = ' '.join(libs)
+
     if build:
       build(bld, obj)
 
