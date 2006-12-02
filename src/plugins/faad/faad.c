b7e50700d1df6f0823edce8f0831e4dcdf0a79c9
Juho Vähä-Herttua <juhovh@iki.fi>
2006-12-02 15:36:38 +0200

    FEATURE(1217): Add replaygain support into faad plugin.

--- src/plugins/faad/faad.c
+++ src/plugins/faad/faad.c
@@ -23,6 +23,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <faad.h>
 #include <glib.h>
 
@@ -505,7 +506,7 @@ xmms_faad_get_mediainfo (xmms_xform_t *xform)
 		if ((temp = mp4ff_meta_get_coverart (data->mp4ff, &metabuf))) {
 			gchar hash[33];
 
-			if (xmms_bindata_plugin_add (metabuf, temp, hash)) {
+			if (xmms_bindata_plugin_add ((guchar *) metabuf, temp, hash)) {
 				xmms_xform_metadata_set_str (xform, XMMS_MEDIALIB_ENTRY_PROPERTY_PICTURE_FRONT, hash);
 				xmms_xform_metadata_set_str (xform, XMMS_MEDIALIB_ENTRY_PROPERTY_PICTURE_FRONT_MIME, "image/jpeg");
 			}
@@ -527,6 +528,42 @@ xmms_faad_get_mediainfo (xmms_xform_t *xform)
 		if (mp4ff_meta_find_by_name (data->mp4ff, "MusicBrainz Artist Id", &metabuf)) {
 			xmms_xform_metadata_set_str (xform,
 			                             XMMS_MEDIALIB_ENTRY_PROPERTY_ARTIST_ID,
+			                             metabuf);
+			g_free (metabuf);
+		}
+
+		/* Replay Gain support */
+		if (mp4ff_meta_find_by_name (data->mp4ff, "replaygain_track_gain", &metabuf)) {
+			gchar buf[8];
+
+			g_snprintf (buf, sizeof (buf), "%f",
+			            pow (10.0, g_strtod (metabuf, NULL) / 20));
+			g_free (metabuf);
+
+			xmms_xform_metadata_set_str (xform,
+			                             XMMS_MEDIALIB_ENTRY_PROPERTY_GAIN_TRACK,
+			                             buf);
+		}
+		if (mp4ff_meta_find_by_name (data->mp4ff, "replaygain_album_gain", &metabuf)) {
+			gchar buf[8];
+
+			g_snprintf (buf, sizeof (buf), "%f",
+			            pow (10.0, g_strtod (metabuf, NULL) / 20));
+			g_free (metabuf);
+
+			xmms_xform_metadata_set_str (xform,
+			                             XMMS_MEDIALIB_ENTRY_PROPERTY_GAIN_ALBUM,
+			                             buf);
+		}
+		if (mp4ff_meta_find_by_name (data->mp4ff, "replaygain_track_peak", &metabuf)) {
+			xmms_xform_metadata_set_str (xform,
+			                             XMMS_MEDIALIB_ENTRY_PROPERTY_PEAK_TRACK,
+			                             metabuf);
+			g_free (metabuf);
+		}
+		if (mp4ff_meta_find_by_name (data->mp4ff, "replaygain_album_peak", &metabuf)) {
+			xmms_xform_metadata_set_str (xform,
+			                             XMMS_MEDIALIB_ENTRY_PROPERTY_PEAK_ALBUM,
 			                             metabuf);
 			g_free (metabuf);
 		}
