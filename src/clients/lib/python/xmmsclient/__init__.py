908af1aebe61ff6f14127d7dcddac8abba9f9137
Anders Gustafsson <andersg@xmms.org>
2007-01-08 19:07:53 +0100

    FEATURE(320): Use explicit imports instead of * in python module __init__

--- src/clients/lib/python/xmmsclient/__init__.py
+++ src/clients/lib/python/xmmsclient/__init__.py
@@ -1,6 +1,6 @@
 ##
 
-from xmmsapi import *
-from sync import *
+from xmmsapi import XMMS, XMMSResult, userconfdir_get
+from sync import XMMSSync, XMMSError
 from propdict import PropDict
 from consts import *
