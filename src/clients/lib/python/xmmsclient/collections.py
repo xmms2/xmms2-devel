c963a9715729915a3f95444b50a3be900fcd137f
Anders Gustafsson <andersg@xmms.org>
2007-01-08 19:05:25 +0100

    FEATURE(320): Add a xmmsclient.collections module.

---
+++ src/clients/lib/python/xmmsclient/collections.py
@@ -0,0 +1,14 @@
+
+from xmmsapi import Reference
+from xmmsapi import Universe
+from xmmsapi import Match
+from xmmsapi import Contains
+from xmmsapi import Smaller
+from xmmsapi import Greater
+from xmmsapi import IDList
+from xmmsapi import Queue
+from xmmsapi import PShuffle
+from xmmsapi import Union
+from xmmsapi import Intersection
+from xmmsapi import Complement
+from xmmsapi import Has
