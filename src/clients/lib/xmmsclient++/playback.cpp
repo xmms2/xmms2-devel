91b7dec0893f97b7f158870cc0a9919b98981e57
Jonne Lehtinen <jonne.lehtinen@tut.fi>
2007-01-16 00:52:09 +0200

    OTHER: C++ bindings playback module nearly converted.

--- src/clients/lib/xmmsclient++/playback.cpp
+++ src/clients/lib/xmmsclient++/playback.cpp
@@ -105,17 +105,13 @@ namespace Xmms
 		return UintResult( res, ml_ );
 	}
 
-	Playback::Status Playback::getStatus() const
+	StatusResult Playback::getStatus() const
 	{
 		xmmsc_result_t* res = 
-		    call( connected_, ml_,
+		    call( connected_,
 		          boost::bind( xmmsc_playback_status, conn_ ) );
 
-		unsigned int status = 0;
-		xmmsc_result_get_uint( res, &status );
-		xmmsc_result_unref( res );
-
-		return static_cast< Playback::Status >(status);
+		return StatusResult( res, ml_ );
 	}
 
 	UintResult Playback::getPlaytime() const
@@ -142,157 +138,6 @@ namespace Xmms
 		          boost::bind( xmmsc_playback_volume_get, conn_ ) );
 		return DictResult( res, ml_ );
 	}
-
-#if 0
-	void Playback::tickle( const VoidSlot& slot,
-	                       const ErrorSlot& error ) const
-	{
-		aCall<void>( connected_, boost::bind( xmmsc_playback_tickle, conn_ ),
-		             slot, error );
-	}
-
-	void Playback::stop( const VoidSlot& slot,
-	                     const ErrorSlot& error ) const
-	{
-		aCall<void>( connected_, boost::bind( xmmsc_playback_stop, conn_ ),
-		             slot, error );
-	}
-
-	void Playback::pause( const VoidSlot& slot,
-	                      const ErrorSlot& error ) const
-	{
-		aCall<void>( connected_, boost::bind( xmmsc_playback_pause, conn_ ),
-		             slot, error );
-	}
-
-	void Playback::start( const VoidSlot& slot,
-	                      const ErrorSlot& error ) const
-	{
-		aCall<void>( connected_, boost::bind( xmmsc_playback_start, conn_ ),
-		             slot, error );
-	}
-
-	void Playback::seekMs( unsigned int milliseconds,
-	                       const VoidSlot& slot,
-	                       const ErrorSlot& error ) const
-	{
-		using boost::bind;
-		aCall<void>( connected_,
-		             bind( xmmsc_playback_seek_ms, conn_, milliseconds ),
-		             slot, error );
-	}
-
-	void Playback::seekMsRel( int milliseconds,
-	                          const VoidSlot& slot,
-	                          const ErrorSlot& error ) const
-	{
-		using boost::bind;
-		aCall<void>( connected_,
-		             bind( xmmsc_playback_seek_ms_rel, conn_, milliseconds ),
-		             slot, error );
-	}
-
-	void Playback::seekSamples( unsigned int samples,
-	                            const VoidSlot& slot,
-	                            const ErrorSlot& error ) const
-	{
-		using boost::bind;
-		aCall<void>( connected_,
-		             bind( xmmsc_playback_seek_samples, conn_, samples ),
-		             slot, error );
-	}
-
-	void Playback::seekSamplesRel( int samples,
-	                               const VoidSlot& slot,
-	                               const ErrorSlot& error ) const
-	{
-		using boost::bind;
-		aCall<void>( connected_,
-		             bind( xmmsc_playback_seek_samples_rel, conn_, samples ),
-		             slot, error );
-	}
-
-	void Playback::currentID( const UintSlot& slot,
-	                          const ErrorSlot& error ) const
-	{
-		aCall<unsigned int>( connected_, 
-		                     boost::bind( xmmsc_playback_current_id, conn_ ),
-		                     slot, error );
-	}
-
-	void Playback::getStatus( const StatusSlot& slot,
-	                          const ErrorSlot& error ) const
-	{
-		aCall<Status>( connected_, boost::bind( xmmsc_playback_status, conn_ ),
-		               slot, error );
-	}
-
-	void Playback::getPlaytime( const UintSlot& slot,
-	                            const ErrorSlot& error ) const
-	{
-		aCall<unsigned int>( connected_,
-		                     boost::bind( xmmsc_playback_playtime, conn_ ),
-		                     slot, error );
-	}
-
-	void Playback::volumeSet( const std::string& channel, unsigned int volume,
-	                          const VoidSlot& slot,
-	                          const ErrorSlot& error ) const
-	{
-		aCall<void>( connected_,
-		             boost::bind( xmmsc_playback_volume_set, conn_,
-		                          channel.c_str(), volume ),
-		             slot, error );
-	}
-
-	void Playback::volumeGet( const DictSlot& slot,
-	                          const ErrorSlot& error ) const
-	{
-		aCall<Dict>( connected_,
-		             boost::bind( xmmsc_playback_volume_get, conn_ ),
-		             slot, error );
-	}
-
-	void
-	Playback::broadcastCurrentID( const UintSlot& slot,
-	                              const ErrorSlot& error ) const
-	{
-		aCall<unsigned int>( connected_,
-		                     boost::bind( xmmsc_broadcast_playback_current_id,
-		                                  conn_ ),
-		                     slot, error );
-	}
-
-	void
-	Playback::broadcastStatus( const StatusSlot& slot,
-	                           const ErrorSlot& error ) const
-	{
-		aCall<Status>( connected_,
-		               boost::bind( xmmsc_broadcast_playback_status,
-		                            conn_ ),
-		               slot, error );
-	}
-
-	void
-	Playback::broadcastVolumeChanged( const DictSlot& slot,
-	                                  const ErrorSlot& error ) const
-	{
-		aCall<Dict>( connected_,
-		             boost::bind( xmmsc_broadcast_playback_volume_changed,
-		                          conn_ ),
-		             slot, error );
-	}
-
-	void
-	Playback::signalPlaytime( const UintSlot& slot,
-	                          const ErrorSlot& error ) const
-	{
-		aCall<unsigned int>( connected_,
-		                     boost::bind( xmmsc_signal_playback_playtime,
-		                                  conn_ ),
-		                     slot, error );
-	}
-#endif
 
 	Playback::Playback( xmmsc_connection_t*& conn, bool& connected,
 	                    MainloopInterface*& ml ) :
