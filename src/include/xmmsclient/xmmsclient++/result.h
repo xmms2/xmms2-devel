013b4e0fe4be79071b1baa60c0a991b1f42215a1
Jonne Lehtinen <jonne.lehtinen@tut.fi>
2007-01-16 11:47:04 +0200

    OTHER: Completed result wrappers for C++ bindings.

--- src/include/xmmsclient/xmmsclient++/result.h
+++ src/include/xmmsclient/xmmsclient++/result.h
@@ -3,6 +3,7 @@
 
 #include <xmmsclient/xmmsclient.h>
 #include <xmmsclient/xmmsclient++/dict.h>
+#include <xmmsclient/xmmsclient++/list.h>
 #include <xmmsclient/xmmsclient++/mainloop.h>
 #include <xmmsclient/xmmsclient++/helpers.h>
 #include <string>
@@ -11,35 +12,43 @@
 namespace Xmms
 {
 
-	template< typename T, typename A, int get( xmmsc_result_t*, A* ) >
-	class Adapter
+	template< typename T >
+	class AdapterBase
 	{
-
 		public:
 
-			Adapter( xmmsc_result_t* res, MainloopInterface*& ml )
-				: res_( res ), value_( 0 ), ml_( ml ), sig_( 0 )
+			AdapterBase( xmmsc_result_t* res, MainloopInterface*& ml )
+				: res_( res ), ml_( ml ), sig_( 0 )
 			{
 			}
 
-			virtual ~Adapter()
+			virtual ~AdapterBase()
 			{
 				xmmsc_result_unref( res_ );
 			}
 
-			operator T()
+			AdapterBase( const AdapterBase& src )
+				: res_( src.res_ ), ml_( src.ml_ )
 			{
-				check( ml_ );
-				xmmsc_result_wait( res_ );
-				check( res_ );
+				xmmsc_result_ref( res_ );
 
-				if( !get( res_, &value_ ) ) {
-					// FIXME: Handle failure
-				}
-				return T( value_ );
+				// Does not delete the signals if it's been added to the
+				// SignalHolder because operator() sets the sig_ to 0.
+				delete sig_;
 			}
 
-			void operator()()
+			AdapterBase&
+			operator=( const AdapterBase& src )
+			{
+				xmmsc_result_unref( res_ );
+				res_ = src.res_;
+				ml_ = src.ml_;
+				xmmsc_result_ref( res_ );
+				return *this;
+			}
+
+			virtual void
+			operator()()
 			{
 				if( sig_ ) {
 					SignalHolder::getInstance().addSignal( sig_ );
@@ -52,21 +61,24 @@ namespace Xmms
 				sig_ = 0;
 			}
 
-			void operator()( boost::function< bool( const T& ) > slot )
+			virtual void
+			operator()( typename Signal<T>::signal_t::slot_type slot )
 			{
 				connect( slot );
 				(*this)();
 			}
 
-			void operator()( boost::function< bool( const T& ) > slot,
-			                 boost::function< bool( const std::string& ) > error )
+			virtual void
+			operator()( typename Signal<T>::signal_t::slot_type slot,
+			            boost::function< bool( const std::string& ) > error )
 			{
 				connect( slot );
 				connectError( error );
 				(*this)();
 			}
 
-			void connect( boost::function< bool( const T& ) > slot )
+			virtual void
+			connect( typename Signal<T>::signal_t::slot_type slot )
 			{
 				if( !sig_ ) {
 					sig_ = new Signal< T >;
@@ -74,7 +86,8 @@ namespace Xmms
 				sig_->signal.connect( slot );
 			}
 
-			void connectError( boost::function< bool( const std::string& ) > error )
+			virtual void
+			connectError( boost::function< bool( const std::string& ) > error )
 			{
 				if( !sig_ ) {
 					sig_ = new Signal< T >;
@@ -82,42 +95,93 @@ namespace Xmms
 				sig_->error_signal.connect( error );
 			}
 
-		private:
+		protected:
 
 			xmmsc_result_t* res_;
-			A value_;
 			MainloopInterface*& ml_;
 			Signal< T >* sig_;
 
 	};
 
+	template< typename T, typename A, int get( xmmsc_result_t*, A* ) >
+	class Adapter : public AdapterBase< T >
+	{
+
+		public:
+
+			Adapter( xmmsc_result_t* res, MainloopInterface*& ml )
+				: AdapterBase<T>( res, ml ), value_( 0 )
+			{
+			}
+
+			virtual ~Adapter()
+			{
+			}
+
+			Adapter( const Adapter& src )
+				: AdapterBase<T>( src ), value_( src.value_ )
+			{
+			}
+
+			Adapter&
+			operator=( const Adapter& src )
+			{
+				AdapterBase<T>::operator=( src );
+				value_ = src.value_;
+				return *this;
+			}
+
+			operator T()
+			{
+				check( this->ml_ );
+				xmmsc_result_wait( this->res_ );
+				check( this->res_ );
+
+				if( !get( this->res_, &value_ ) ) {
+					// FIXME: Handle failure
+				}
+				return T( value_ );
+			}
+
+		private:
+
+			A value_;
+
+	};
+
 	template< typename T >
-	class ClassAdapter
+	class ClassAdapter : public AdapterBase< T >
 	{
 
 		public:
 
 			ClassAdapter( xmmsc_result_t* res, MainloopInterface*& ml )
-				: res_( res ), ml_( ml )
+				: AdapterBase<T>( res, ml )
 			{
 			}
 
-			virtual ~ClassAdapter()
+			~ClassAdapter()
 			{
-				xmmsc_result_unref( res_ );
+			}
+
+			ClassAdapter( const ClassAdapter& src )
+				: AdapterBase<T>( src )
+			{
+			}
+
+			ClassAdapter&
+			operator=( const ClassAdapter& src )
+			{
+				AdapterBase<T>::operator=( src );
+				return *this;
 			}
 
 			operator T()
 			{
-				check( ml_ );
-				xmmsc_result_wait( res_ );
-				return T( res_ );
+				check( this->ml_ );
+				xmmsc_result_wait( this->res_ );
+				return T( this->res_ );
 			}
-
-		private:
-
-			xmmsc_result_t* res_;
-			MainloopInterface*& ml_;
 
 	};
 
@@ -127,16 +191,21 @@ namespace Xmms
 	typedef Adapter< std::string, char*, xmmsc_result_get_string > StringResult;
 	typedef Adapter< xmms_playback_status_t, uint32_t,
 	                 xmmsc_result_get_uint > StatusResult;
+
 	typedef ClassAdapter< Dict > DictResult;
 	typedef ClassAdapter< PropDict > PropDictResult;
+	typedef ClassAdapter< List< int > > IntListResult;
+	typedef ClassAdapter< List< unsigned int > > UintListResult;
+	typedef ClassAdapter< List< std::string > > StringListResult;
+	typedef ClassAdapter< List< Dict > > DictListResult;
 
-	class VoidResult
+	class VoidResult : public AdapterBase< void >
 	{
 
 		public:
 
 			VoidResult( xmmsc_result_t* res, MainloopInterface*& ml )
-				: res_( res )
+				: AdapterBase<void>( res, ml )
 			{
 				if( !(ml && ml->isRunning()) ) {
 					xmmsc_result_wait( res );
@@ -146,19 +215,61 @@ namespace Xmms
 
 			~VoidResult()
 			{
-				xmmsc_result_unref( res_ );
 			}
 
-			void operator()()
+			VoidResult( const VoidResult& src )
+				: AdapterBase<void>( src )
 			{
-				xmmsc_result_notifier_set( res_,
-				                           Xmms::generic_callback< void >,
-				                           0 );
 			}
 
-		private:
+			VoidResult&
+			operator=( const VoidResult& src )
+			{
+				AdapterBase<void>::operator=( src );
+				return *this;
+			}
 
-			xmmsc_result_t* res_;
+	};
+
+	class BinResult : public AdapterBase< bin >
+	{
+
+		public:
+
+			BinResult( xmmsc_result_t* res, MainloopInterface*& ml )
+				: AdapterBase< bin >( res, ml )
+			{
+			}
+
+			~BinResult()
+			{
+			}
+
+			BinResult( const BinResult& src )
+				: AdapterBase< bin >( src )
+			{
+			}
+
+			BinResult&
+			operator=( const BinResult& src )
+			{
+				AdapterBase<bin>::operator=( src );
+				return *this;
+			}
+
+			operator bin()
+			{
+				check( this->ml_ );
+				xmmsc_result_wait( this->res_ );
+				check( this->res_ );
+
+				unsigned char* temp = 0;
+				unsigned int len = 0;
+				if( !xmmsc_result_get_bin( this->res_, &temp, &len ) ) {
+					// FIXME: Handle failure
+				}
+				return Xmms::bin( temp, len );
+			}
 
 	};
 
