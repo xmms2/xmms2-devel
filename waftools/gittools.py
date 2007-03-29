bc177d920458acc1222be7a9810c24e65ed0abe5
Anders Gustafsson <andersg@xmms.org>
2007-03-29 16:44:24 +0200

    BUG(1465): Hide "fatal: Not a git repository" error message.

--- waftools/gittools.py
+++ waftools/gittools.py
@@ -9,7 +9,9 @@ def gitsha(path):
 	return h.hexdigest()
 
 def git_info():
-	commithash = os.popen('git-rev-parse --verify HEAD').read().strip()
+	commithash = os.popen('git-rev-parse --verify HEAD 2>/dev/null').read().strip()
+	if not commithash:
+		raise ValueError("Couldn't get hash")
 	if os.getuid() == os.stat(".git/index").st_uid:
 		os.system('git-update-index --refresh >/dev/null')
 	else:
