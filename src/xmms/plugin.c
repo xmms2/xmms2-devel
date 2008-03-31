b49ae7cef1b37448376970542e7e3300567b5f18
Florian Ragwitz <rafl@debian.org>
2008-03-31 14:50:21 +0200

    OTHER: Whitespace fix in src/xmms/plugin.c.

--- src/xmms/plugin.c
+++ src/xmms/plugin.c
@@ -391,7 +391,7 @@ xmms_plugin_client_list_foreach (xmms_plugin_t *plugin, gpointer data)
 {
 	GHashTable *hash;
 	GList **list = data;
-	
+
 	hash = g_hash_table_new_full (g_str_hash, g_str_equal, NULL,
 	                              (GDestroyNotify)xmms_object_cmd_value_unref);
 	g_hash_table_insert (hash, "name",
