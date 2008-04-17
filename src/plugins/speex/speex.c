4da19e342bdaf1f82872c07ae67ee720f0634e80
Tilman Sauerbeck <tilman@xmms.org>
2008-04-17 21:26:23 +0200

    BUG(1942): Ported the Speex plugin to the xform API.

--- src/plugins/speex/speex.c
+++ src/plugins/speex/speex.c
@@ -14,7 +14,8 @@
  *  Lesser General Public License for more details.
  */
 
-#include "xmms/xmms_decoderplugin.h"
+#include "xmms/xmms_xformplugin.h"
+#include "xmms/xmms_config.h"
 #include "xmms/xmms_log.h"
 
 #include <string.h>
@@ -36,93 +37,77 @@ typedef struct xmms_speex_data_St {
 	ogg_page ogg_page;
 	ogg_packet ogg_packet;
 	gchar *ogg_data;
+
+	gint16 *samples_buf;
+	gint16 *samples_start;
+	gint samples_count;
 } xmms_speex_data_t;
 
 /*
  * Function prototypes
  */
 
-static gboolean xmms_speex_new (xmms_decoder_t *decoder);
-static gboolean xmms_speex_init (xmms_decoder_t *decoder, gint mode);
-static gboolean xmms_speex_seek (xmms_decoder_t *decoder, guint samples);
-static gboolean xmms_speex_decode_block (xmms_decoder_t *decoder);
-static void xmms_speex_destroy (xmms_decoder_t *decoder);
-static void xmms_speex_get_mediainfo (xmms_decoder_t *decoder);
+static gboolean xmms_speex_plugin_setup (xmms_xform_plugin_t *xform_plugin);
+static gboolean xmms_speex_init (xmms_xform_t *xform);
+static gint xmms_speex_read (xmms_xform_t *xform, gpointer buf, gint len, xmms_error_t *err);
+static void xmms_speex_destroy (xmms_xform_t *xform);
+static void xmms_speex_read_metadata (xmms_xform_t *xform, xmms_speex_data_t *data);
 
 /*
  * Plugin header
  */
 
-xmms_plugin_t *
-xmms_plugin_get (void)
-{
-	xmms_plugin_t *plugin;
-
-	plugin = xmms_plugin_new (XMMS_PLUGIN_TYPE_DECODER,
-	                          XMMS_DECODER_PLUGIN_API_VERSION,
-	                          "speex",
-	                          "Speex Decoder",
-	                          XMMS_VERSION,
-	                          "Speex decoder");
-
-	if (!plugin) {
-		return NULL;
-	}
-
-	xmms_plugin_info_add (plugin, "URL", "http://www.speex.org/");
-	xmms_plugin_info_add (plugin, "Author", "XMMS Team");
-
-	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_NEW, xmms_speex_new);
-	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_INIT, xmms_speex_init);
-	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_SEEK, xmms_speex_seek);
-	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_DESTROY, xmms_speex_destroy);
-	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_DECODE_BLOCK, xmms_speex_decode_block);
-	xmms_plugin_method_add (plugin, XMMS_PLUGIN_METHOD_GET_MEDIAINFO, xmms_speex_get_mediainfo);
-
-	//xmms_plugin_properties_add (plugin, XMMS_PLUGIN_PROPERTY_FAST_FWD);
-	//xmms_plugin_properties_add (plugin, XMMS_PLUGIN_PROPERTY_REWIND);
-
-	xmms_plugin_config_property_register (plugin, "perceptual_enhancer", "1",
-	                                      NULL, NULL);
-
-	xmms_plugin_magic_add (plugin, "ogg/speex header", "audio/x-speex",
-	                       "0 string OggS", ">4 byte 0",
-	                       ">>28 string Speex   ", NULL);
-
-	return plugin;
-}
+XMMS_XFORM_PLUGIN ("speex",
+                   "Speex Decoder", XMMS_VERSION,
+                   "Speex decoder",
+                   xmms_speex_plugin_setup);
 
 static gboolean
-xmms_speex_new (xmms_decoder_t *decoder)
+xmms_speex_plugin_setup (xmms_xform_plugin_t *xform_plugin)
 {
-	xmms_speex_data_t *data;
+	xmms_xform_methods_t methods;
 
-	g_return_val_if_fail (decoder, FALSE);
+	XMMS_XFORM_METHODS_INIT (methods);
 
-	data = g_new0 (xmms_speex_data_t, 1);
+	methods.init = xmms_speex_init;
+	methods.destroy = xmms_speex_destroy;
+	methods.read = xmms_speex_read;
 
-	xmms_decoder_private_data_set (decoder, data);
+	xmms_xform_plugin_methods_set (xform_plugin, &methods);
+
+	xmms_xform_plugin_indata_add (xform_plugin,
+	                              XMMS_STREAM_TYPE_MIMETYPE,
+	                              "audio/x-speex",
+	                              NULL);
+
+	xmms_magic_add ("ogg/speex header", "audio/x-speex",
+	                "0 string OggS", ">4 byte 0",
+	                ">>28 string Speex   ", NULL);
+
+	xmms_magic_extension_add ("audio/x-speex", "*.spx");
+
+	xmms_xform_plugin_config_property_register (xform_plugin,
+	                                            "perceptual_enhancer",
+	                                            "1", NULL, NULL);
 
 	return TRUE;
 }
 
 static gboolean
-xmms_speex_init (xmms_decoder_t *decoder, gint mode)
+xmms_speex_init (xmms_xform_t *xform)
 {
 	gint pe;
 
 	xmms_config_property_t *val;
 	xmms_speex_data_t *data;
 	xmms_error_t error;
-	xmms_transport_t *transport;
 
-	g_return_val_if_fail (decoder, FALSE);
+	g_return_val_if_fail (xform, FALSE);
 
-	data = xmms_decoder_private_data_get (decoder);
+	data = g_new0 (xmms_speex_data_t, 1);
 	g_return_val_if_fail (data, FALSE);
 
-	transport = xmms_decoder_transport_get (decoder);
-	g_return_val_if_fail (transport, FALSE);
+	xmms_xform_private_data_set (xform, data);
 
 	ogg_sync_init (&data->sync_state);
 	speex_bits_init (&data->speex_bits);
@@ -133,7 +118,7 @@ xmms_speex_init (xmms_decoder_t *decoder, gint mode)
 		gint ret;
 
 		data->ogg_data = ogg_sync_buffer (&data->sync_state, 1024);
-		ret = xmms_transport_read (transport, data->ogg_data, 1024, &error);
+		ret = xmms_xform_read (xform, data->ogg_data, 1024, &error);
 		ogg_sync_wrote (&data->sync_state, ret);
 
 		if (ret <= 0) {
@@ -159,8 +144,7 @@ xmms_speex_init (xmms_decoder_t *decoder, gint mode)
 	                                            data->ogg_packet.bytes);
 	data->speex_state = speex_decoder_init (speex_mode_list[data->speexheader->mode]);
 
-	val = xmms_plugin_config_lookup (xmms_decoder_plugin_get (decoder),
-	                                 "perceptual_enhancer");
+	val = xmms_xform_config_lookup (xform, "perceptual_enhancer");
 	pe = xmms_config_property_get_int (val);
 	speex_decoder_ctl (data->speex_state, SPEEX_SET_ENH, &pe);
 
@@ -168,101 +152,134 @@ xmms_speex_init (xmms_decoder_t *decoder, gint mode)
 	ogg_stream_pagein (&data->stream_state, &data->ogg_page);
 	ogg_stream_packetout (&data->stream_state, &data->ogg_packet);
 
-	xmms_decoder_format_add (decoder, XMMS_SAMPLE_FORMAT_S16,
-	                         data->speexheader->nb_channels,
-	                         data->speexheader->rate);
-	/* we don't have to care about the return value other than NULL,
-	   as there is only one format (to rule them all) */
-	if (xmms_decoder_format_finish (decoder) == NULL) {
-		return FALSE;
-	}
+	data->samples_buf = g_new (gint16,
+	                           data->speexheader->frames_per_packet *
+	                           data->speexheader->frame_size *
+	                           data->speexheader->nb_channels);
+	data->samples_start = data->samples_buf;
+	data->samples_count = 0;
+
+	xmms_speex_read_metadata (xform, data);
+
+	xmms_xform_outdata_type_add (xform,
+	                             XMMS_STREAM_TYPE_MIMETYPE,
+	                             "audio/pcm",
+	                             XMMS_STREAM_TYPE_FMT_FORMAT,
+	                             XMMS_SAMPLE_FORMAT_S16,
+	                             XMMS_STREAM_TYPE_FMT_CHANNELS,
+	                             data->speexheader->nb_channels,
+	                             XMMS_STREAM_TYPE_FMT_SAMPLERATE,
+	                             data->speexheader->rate,
+	                             XMMS_STREAM_TYPE_END);
 
 	return TRUE;
 }
 
-static gboolean
-xmms_speex_seek (xmms_decoder_t *decoder, guint samples)
+static gint
+xmms_speex_read (xmms_xform_t *xform, gpointer buf, gint len,
+                 xmms_error_t *err)
 {
-	g_return_val_if_fail (decoder, FALSE);
-
-	return FALSE; /* Seeking not supported right now */
-}
-
-static gboolean
-xmms_speex_decode_block (xmms_decoder_t *decoder)
-{
-	gint ret;
+	gint ret = 0, n;
 	gfloat outfloat [2000];
-	gint16 outshort [2000];
+	gint16 *outbuf = (gint16 *) buf;
 	xmms_speex_data_t *data;
 	xmms_error_t error;
-	xmms_transport_t *transport;
 	SpeexStereoState stereo = SPEEX_STEREO_STATE_INIT;
 
-	g_return_val_if_fail (decoder, FALSE);
+	g_return_val_if_fail (xform, -1);
 
-	data = xmms_decoder_private_data_get (decoder);
-	g_return_val_if_fail (data, FALSE);
+	data = xmms_xform_private_data_get (xform);
+	g_return_val_if_fail (data, -1);
 
-	transport = xmms_decoder_transport_get (decoder);
-	g_return_val_if_fail (transport, FALSE);
+	/* convert from bytes to samples */
+	len /= 2;
 
-	if (ogg_stream_packetout (&data->stream_state, &data->ogg_packet) > 0) {
-		gint frame, cnt;
+	/* first, copy already decoded samples over if we have any. */
+	if (data->samples_count) {
+		n = MIN (data->samples_count, len);
 
-		speex_bits_read_from (&data->speex_bits,
-		                      data->ogg_packet.packet,
-		                      data->ogg_packet.bytes);
+		memcpy (outbuf, data->samples_start, n * 2);
+		data->samples_count -= n;
 
-		for (frame = 0; frame < data->speexheader->frames_per_packet; frame++) {
-
-			speex_decode (data->speex_state, &data->speex_bits, outfloat);
-
-			if (data->speexheader->nb_channels == 2) {
-				speex_decode_stereo (outfloat, data->speexheader->frame_size,&stereo);
-			}
-
-			for (cnt = 0; cnt < data->speexheader->frame_size *
-			                    data->speexheader->nb_channels; cnt++) {
-				outshort [cnt] = outfloat [cnt];
-			}
-
-			xmms_decoder_write (decoder, (gchar *) outshort,
-			                    data->speexheader->frame_size *
-			                    data->speexheader->nb_channels * 2);
+		if (!data->samples_count) {
+			data->samples_start = data->samples_buf;
+		} else {
+			data->samples_start += n;
 		}
 
-		return TRUE;
+		/* convert from samples to bytes */
+		return n * 2;
 	}
 
-	/* Need more data */
+	while (42) {
+		gint samples_per_frame;
 
-	data->ogg_data = ogg_sync_buffer (&data->sync_state, 200);
-	ret = xmms_transport_read (transport, data->ogg_data, 200, &error);
-	ogg_sync_wrote (&data->sync_state, ret);
+		samples_per_frame = data->speexheader->frame_size *
+		                    data->speexheader->nb_channels;
 
-	if ( ret <= 0 ) {
-		return FALSE;
+		while (ogg_stream_packetout (&data->stream_state, &data->ogg_packet) == 1) {
+			gint frame;
+
+			speex_bits_read_from (&data->speex_bits,
+			                      data->ogg_packet.packet,
+			                      data->ogg_packet.bytes);
+
+			for (frame = 0; frame < data->speexheader->frames_per_packet; frame++) {
+				gint cnt;
+
+				speex_decode (data->speex_state, &data->speex_bits, outfloat);
+
+				if (data->speexheader->nb_channels == 2) {
+					speex_decode_stereo (outfloat, data->speexheader->frame_size,&stereo);
+				}
+
+				n = MIN (samples_per_frame, len);
+
+				/* copy as many samples to the output buffer as
+				 * possible.
+				 */
+				for (cnt = 0; cnt < n; cnt++) {
+					*outbuf++ = outfloat[cnt];
+					len--;
+					ret += 2;
+				}
+
+				/* store the remaining samples for later use */
+				for (; cnt < samples_per_frame; cnt++) {
+					data->samples_buf[data->samples_count++] =
+						outfloat[cnt];
+				}
+			}
+
+			return ret;
+		}
+
+		/* Need more data */
+
+		do {
+			gint ret;
+
+			data->ogg_data = ogg_sync_buffer (&data->sync_state, 200);
+			ret = xmms_xform_read (xform, data->ogg_data, 200, &error);
+			ogg_sync_wrote (&data->sync_state, ret);
+
+			if (ret <= 0) {
+				return ret;
+			}
+		} while (ogg_sync_pageout (&data->sync_state, &data->ogg_page) != 1);
+
+		ogg_stream_pagein (&data->stream_state, &data->ogg_page);
 	}
-
-	if (ogg_sync_pageout (&data->sync_state, &data->ogg_page) < 1) {
-		return TRUE;
-	}
-
-	if (ogg_stream_pagein (&data->stream_state, &data->ogg_page) != 0) {
-		return TRUE;
-	}
-
-	return TRUE;
 }
 
 static void
-xmms_speex_destroy (xmms_decoder_t *decoder)
+xmms_speex_destroy (xmms_xform_t *xform)
 {
 	xmms_speex_data_t *data;
 
-	g_return_if_fail (decoder);
-	data = xmms_decoder_private_data_get (decoder);
+	g_return_if_fail (xform);
+
+	data = xmms_xform_private_data_get (xform);
 	g_return_if_fail (data);
 
 	ogg_stream_clear (&data->stream_state);
@@ -271,30 +288,16 @@ xmms_speex_destroy (xmms_decoder_t *decoder)
 	speex_decoder_destroy (data->speex_state);
 	speex_bits_destroy (&data->speex_bits);
 
+	g_free (data->samples_buf);
 	g_free (data->speexheader);
 	g_free (data);
 }
 
 static void
-xmms_speex_get_mediainfo (xmms_decoder_t *decoder)
+xmms_speex_read_metadata (xmms_xform_t *xform, xmms_speex_data_t *data)
 {
-	xmms_medialib_entry_t entry;
-	xmms_speex_data_t *data;
-
-	g_return_if_fail (decoder);
-
-	if (!xmms_speex_init (decoder, 0))
-		return;
-
-	data = xmms_decoder_private_data_get (decoder);
-	g_return_if_fail (data);
-
-	entry = xmms_medialib_entry_new (NULL);
-
-	xmms_medialib_entry_property_set_int (entry,
-	                                      XMMS_MEDIALIB_ENTRY_PROPERTY_BITRATE,
-	                                      data->speexheader->bitrate);
-
-	xmms_medialib_entry_send_update (entry);
+	xmms_xform_metadata_set_int (xform,
+	                             XMMS_MEDIALIB_ENTRY_PROPERTY_BITRATE,
+	                             data->speexheader->bitrate);
 }
 
