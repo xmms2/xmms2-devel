94df830fc2e81a7b8be0c4ec6bc22fb6339ef013
Florian Ragwitz <rafl@debian.org>
2008-06-09 02:13:11 +0200

    OTHER: Relicense the samba plugin under GPL >= 2.
    It used to be LGPL which isn't possible as it's linking against
    libsmbclient, which is GPL.

--- src/plugins/samba/samba.c
+++ src/plugins/samba/samba.c
@@ -4,14 +4,14 @@
  *  PLUGINS ARE NOT CONSIDERED TO BE DERIVED WORK !!!
  *
  *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
  *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ *  General Public License for more details.
  */
 
 /* smbclient might be compiled with this,
