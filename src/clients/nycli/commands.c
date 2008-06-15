45d92ca44cf178383e8e9272c90c864b1d746460
Igor Assis <igor.assis@gmail.com>
2008-06-15 11:29:45 +0000

    OTHER: Command 'seek' was using infos->conn instead of sync

--- src/clients/nycli/commands.c
+++ src/clients/nycli/commands.c
@@ -412,9 +412,9 @@ cli_seek (cli_infos_t *infos, command_context_t *ctx)
 
 	if (command_arg_time_get (ctx, 0, &t)) {
 		if (t.type == COMMAND_ARG_TIME_OFFSET) {
-			res = xmmsc_playback_seek_ms_rel (infos->conn, t.value.offset * 1000);
+			res = xmmsc_playback_seek_ms_rel (infos->sync, t.value.offset * 1000);
 		} else {
-			res = xmmsc_playback_seek_ms (infos->conn, t.value.pos * 1000);
+			res = xmmsc_playback_seek_ms (infos->sync, t.value.pos * 1000);
 		}
 		
  		xmmsc_result_wait (res); 
