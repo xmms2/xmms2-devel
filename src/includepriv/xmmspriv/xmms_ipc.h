21111ffb59ab70904ca19980f7e8f77863ccdd09
Anders Waldenborg <andersg@xmms.org>
2009-01-05 21:11:39 +0100

    OTHER: Remove unused enum.

--- src/includepriv/xmmspriv/xmms_ipc.h
+++ src/includepriv/xmmspriv/xmms_ipc.h
@@ -3,10 +3,6 @@
 
 #include "xmms/xmms_ipc.h"
 
-typedef enum {
-	XMMS_IPC_CLIENT_STATUS_NEW,
-} xmms_ipc_client_status_t;
-
 typedef struct xmms_ipc_St xmms_ipc_t;
 
 xmms_ipc_t *xmms_ipc_init (void);
