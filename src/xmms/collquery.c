xmms2/xmms2-devel

commit c0c276599ac470c9136653736a8b3fe7b45ee462

OTHER: Remove a number of deprecation warnings

src/xmms/collquery.c
@@ -220,6 +220,8 @@ xmms_collection_append_to_query (xmms_coll_dag_t *dag, xmmsv_coll_t *coll,
 	guint *idlist;
 	gchar *attr1, *attr2, *attr3;
 	gboolean case_sens;
+	xmmsv_list_iter_t *iter;
+	xmmsv_t *tmp;
 
 	xmmsv_coll_type_t type = xmmsv_coll_get_type (coll);
 	switch (type) {
@@ -237,9 +239,11 @@ xmms_collection_append_to_query (xmms_coll_dag_t *dag, xmmsv_coll_t *coll,
 		i = 0;
 		query_append_string (query, "(");
 
-		xmmsv_coll_operand_list_save (coll);
-		xmmsv_coll_operand_list_first (coll);
-		while (xmmsv_coll_operand_list_entry (coll, &op)) {
+		xmmsv_get_list_iter (xmmsv_coll_operands_get (coll), &iter);
+
+		for (xmmsv_list_iter_first (iter);
+		     xmmsv_list_iter_valid (iter);
+		     xmmsv_list_iter_next (iter)) {
 			if (i != 0) {
 				if (type == XMMS_COLLECTION_TYPE_UNION)
 					query_append_string (query, " OR ");
@@ -248,10 +252,11 @@ xmms_collection_append_to_query (xmms_coll_dag_t *dag, xmmsv_coll_t *coll,
 			} else {
 				i = 1;
 			}
+			xmmsv_list_iter_entry (iter, &tmp);
+			xmmsv_get_coll (tmp, &op);
 			xmms_collection_append_to_query (dag, op, query);
-			xmmsv_coll_operand_list_next (coll);
 		}
-		xmmsv_coll_operand_list_restore (coll);
+		xmmsv_list_iter_explicit_destroy (iter);
 
 		query_append_string (query, ")");
 		break;
@@ -406,10 +411,11 @@ query_append_operand (coll_query_t *query, xmms_coll_dag_t *dag, xmmsv_coll_t *c
 	gchar *target_name;
 	gchar *target_ns;
 	guint  target_nsid;
+	xmmsv_t *tmp;
 
-	xmmsv_coll_operand_list_save (coll);
-	xmmsv_coll_operand_list_first (coll);
-	if (!xmmsv_coll_operand_list_entry (coll, &op)) {
+	if (xmmsv_list_get (xmmsv_coll_operands_get (coll), 0, &tmp)) {
+		xmmsv_get_coll (tmp, &op);
+
 		/* Ref'd coll not saved as operand, look for it */
 		if (xmmsv_coll_attribute_get (coll, "reference", &target_name) &&
 		    xmmsv_coll_attribute_get (coll, "namespace", &target_ns)) {
@@ -418,7 +424,6 @@ query_append_operand (coll_query_t *query, xmms_coll_dag_t *dag, xmmsv_coll_t *c
 			op = xmms_collection_get_pointer (dag, target_name, target_nsid);
 		}
 	}
-	xmmsv_coll_operand_list_restore (coll);
 
 	/* Append reference operator */
 	if (op != NULL) {
@@ -435,16 +440,16 @@ query_append_intersect_operand (coll_query_t *query, xmms_coll_dag_t *dag,
                                 xmmsv_coll_t *coll)
 {
 	xmmsv_coll_t *op;
+	xmmsv_t *tmp;
 
-	xmmsv_coll_operand_list_save (coll);
-	xmmsv_coll_operand_list_first (coll);
-	if (xmmsv_coll_operand_list_entry (coll, &op)) {
+	if (xmmsv_list_get (xmmsv_coll_operands_get (coll), 0, &tmp)) {
+		xmmsv_get_coll (tmp, &op);
+
 		if (!operator_is_allmedia (op)) {
 			query_append_string (query, " AND ");
 			xmms_collection_append_to_query (dag, op, query);
 		}
 	}
-	xmmsv_coll_operand_list_restore (coll);
 }
 
 /* Append a filtering clause on the field value, depending on the operator type. */

src/xmms/collection.c
@@ -1106,16 +1106,7 @@ xmms_collection_validate_recurs (xmms_coll_dag_t *dag, xmmsv_coll_t *coll,
 	xmms_collection_namespace_id_t nsid;
 
 	/* count operands */
-	xmmsv_coll_operand_list_save (coll);
-
-	xmmsv_coll_operand_list_first (coll);
-	while (xmmsv_coll_operand_list_entry (coll, &op)) {
-		num_operands++;
-		xmmsv_coll_operand_list_next (coll);
-	}
-
-	xmmsv_coll_operand_list_restore (coll);
-
+	num_operands = xmmsv_list_get_size (xmmsv_coll_operands_get (coll));
 
 	/* analyse by type */
 	type = xmmsv_coll_get_type (coll);
@@ -1173,10 +1164,9 @@ xmms_collection_validate_recurs (xmms_coll_dag_t *dag, xmmsv_coll_t *coll,
 
 		/* ensure that the operand is consistent with the reference infos */
 		if (num_operands == 1) {
-			xmmsv_coll_operand_list_save (coll);
-			xmmsv_coll_operand_list_first (coll);
-			xmmsv_coll_operand_list_entry (coll, &op);
-			xmmsv_coll_operand_list_restore (coll);
+			xmmsv_t *val;
+			xmmsv_list_get (xmmsv_coll_operands_get (coll), 0, &val);
+			xmmsv_get_coll (val, &op);
 
 			if (op != ref) {
 				return FALSE;
@@ -1261,18 +1251,24 @@ xmms_collection_validate_recurs (xmms_coll_dag_t *dag, xmmsv_coll_t *coll,
 
 	/* recurse in operands */
 	if (num_operands > 0 && type != XMMS_COLLECTION_TYPE_REFERENCE) {
-		xmmsv_coll_operand_list_save (coll);
+		xmmsv_list_iter_t *iter;
+		xmmsv_get_list_iter (xmmsv_coll_operands_get (coll), &iter);
 
-		xmmsv_coll_operand_list_first (coll);
-		while (xmmsv_coll_operand_list_entry (coll, &op) && valid) {
+		for (xmmsv_list_iter_first (iter);
+		     valid && xmmsv_list_iter_valid (iter);
+		     xmmsv_list_iter_next (iter)) {
+
+			xmmsv_t *val;
+			xmmsv_list_iter_entry (iter, &val);
+			xmmsv_get_coll (val, &op);
+
 			if (!xmms_collection_validate_recurs (dag, op, save_name,
 			                                      save_namespace)) {
 				valid = FALSE;
 			}
-			xmmsv_coll_operand_list_next (coll);
 		}
 
-		xmmsv_coll_operand_list_restore (coll);
+		xmmsv_list_iter_explicit_destroy (iter);
 	}
 
 	return valid;
@@ -1464,17 +1460,25 @@ xmms_collection_apply_to_collection_recurs (xmms_coll_dag_t *dag,
 	/* Apply the function to the operator. */
 	f (dag, coll, parent, udata);
 
-	/* Recurse into the parents (if not a reference) */
+	/* Recurse into the operands (if not a reference) */
 	if (xmmsv_coll_get_type (coll) != XMMS_COLLECTION_TYPE_REFERENCE) {
-		xmmsv_coll_operand_list_save (coll);
+		xmmsv_list_iter_t *iter;
+		xmmsv_get_list_iter (xmmsv_coll_operands_get (coll), &iter);
 
-		xmmsv_coll_operand_list_first (coll);
-		while (xmmsv_coll_operand_list_entry (coll, &op)) {
-			xmms_collection_apply_to_collection_recurs (dag, op, coll, f, udata);
-			xmmsv_coll_operand_list_next (coll);
+		for (xmmsv_list_iter_first (iter);
+		     xmmsv_list_iter_valid (iter);
+		     xmmsv_list_iter_next (iter)) {
+
+			xmmsv_t *val;
+			xmmsv_list_iter_entry (iter, &val);
+
+			xmmsv_get_coll (val, &op);
+
+			xmms_collection_apply_to_collection_recurs (dag, op, coll, f,
+			                                            udata);
 		}
 
-		xmmsv_coll_operand_list_restore (coll);
+		xmmsv_list_iter_explicit_destroy (iter);
 	}
 }
 
@@ -1622,15 +1626,21 @@ strip_references (xmms_coll_dag_t *dag, xmmsv_coll_t *coll, xmmsv_coll_t *parent
 	coll_rebind_infos_t *infos;
 	gchar *target_name = NULL;
 	gchar *target_namespace = NULL;
+	xmmsv_list_iter_t *iter;
+	xmmsv_t *tmp;
 
 	infos = (coll_rebind_infos_t*)udata;
 
-	xmmsv_coll_operand_list_save (coll);
-	xmmsv_coll_operand_list_first (coll);
-	while (xmmsv_coll_operand_list_entry (coll, &op)) {
+	xmmsv_get_list_iter (xmmsv_coll_operands_get (coll), &iter);
+	for (xmmsv_list_iter_first (iter);
+	     xmmsv_list_iter_valid (iter);
+	     xmmsv_list_iter_next (iter)) {
+
+		xmmsv_list_iter_entry (iter, &tmp);
+		xmmsv_get_coll (tmp, &op);
+
 		/* Skip if not potential reference */
 		if (xmmsv_coll_get_type (op) != XMMS_COLLECTION_TYPE_REFERENCE) {
-			xmmsv_coll_operand_list_next (coll);
 			continue;
 		}
 
@@ -1638,19 +1648,20 @@ strip_references (xmms_coll_dag_t *dag, xmmsv_coll_t *coll, xmmsv_coll_t *parent
 		xmmsv_coll_attribute_get (op, "namespace", &target_namespace);
 		if (strcmp (infos->name, target_name) != 0 ||
 		    strcmp (infos->namespace, target_namespace) != 0) {
-			xmmsv_coll_operand_list_next (coll);
 			continue;
 		}
 
 		/* Rebind coll to ref'd operand directly, effectively strip reference */
-		xmmsv_coll_remove_operand (op, infos->oldtarget);
+		/* FIXME: Do we really need to do this _clear? */
+		xmmsv_list_clear (xmmsv_coll_operands_get (op));
 
-		xmmsv_coll_remove_operand (coll, op);
-		xmmsv_coll_add_operand (coll, infos->oldtarget);
+		xmmsv_list_iter_remove (iter);
 
-		xmmsv_coll_operand_list_first (coll); /* Restart if oplist changed */
+		tmp = xmmsv_new_coll (infos->oldtarget);
+		xmmsv_list_iter_insert (iter, tmp);
+		xmmsv_unref (tmp);
 	}
-	xmmsv_coll_operand_list_restore (coll);
+	xmmsv_list_iter_explicit_destroy (iter);
 }
 
 /**
@@ -1671,14 +1682,14 @@ check_for_reference (xmms_coll_dag_t *dag, xmmsv_coll_t *coll, xmmsv_coll_t *par
 			check->found = TRUE;
 		} else {
 			xmmsv_coll_t *op;
-			xmmsv_coll_operand_list_save (coll);
-			xmmsv_coll_operand_list_first (coll);
-			if (xmmsv_coll_operand_list_entry (coll, &op)) {
+			xmmsv_t *tmp;
+
+			if (xmmsv_list_get (xmmsv_coll_operands_get (coll), 0, &tmp)) {
+				xmmsv_get_coll (tmp, &op);
 				xmms_collection_apply_to_collection_recurs (dag, op, coll,
 				                                            check_for_reference,
 				                                            udata);
 			}
-			xmmsv_coll_operand_list_restore (coll);
 		}
 	}
 }
@@ -1755,6 +1766,7 @@ xmms_collection_media_match (xmms_coll_dag_t *dag, GHashTable *mediainfo,
 	guint32 *idlist;
 	gint i;
 	gint id;
+	xmmsv_list_iter_t *iter;
 
 	switch (xmmsv_coll_get_type (coll)) {
 	case XMMS_COLLECTION_TYPE_REFERENCE:
@@ -1772,27 +1784,37 @@ xmms_collection_media_match (xmms_coll_dag_t *dag, GHashTable *mediainfo,
 
 	case XMMS_COLLECTION_TYPE_UNION:
 		/* if ANY matches */
-		xmmsv_coll_operand_list_save (coll);
-		xmmsv_coll_operand_list_first (coll);
-		while (!match && xmmsv_coll_operand_list_entry (coll, &op)) {
+		xmmsv_get_list_iter (xmmsv_coll_operands_get (coll), &iter);
+
+		for (xmmsv_list_iter_first (iter);
+		     !match && xmmsv_list_iter_valid (iter);
+		     xmmsv_list_iter_next (iter)) {
+
+			xmmsv_list_iter_entry (iter, &val);
+			xmmsv_get_coll (val, &op);
+
 			match = xmms_collection_media_match (dag, mediainfo, op,
 			                                     nsid, match_table);
-			xmmsv_coll_operand_list_next (coll);
 		}
-		xmmsv_coll_operand_list_restore (coll);
+		xmmsv_list_iter_explicit_destroy (iter);
 		break;
 
 	case XMMS_COLLECTION_TYPE_INTERSECTION:
 		/* if ALL match */
 		match = TRUE;
-		xmmsv_coll_operand_list_save (coll);
-		xmmsv_coll_operand_list_first (coll);
-		while (match && xmmsv_coll_operand_list_entry (coll, &op)) {
+		xmmsv_get_list_iter (xmmsv_coll_operands_get (coll), &iter);
+
+		for (xmmsv_list_iter_first (iter);
+		     match && xmmsv_list_iter_valid (iter);
+		     xmmsv_list_iter_next (iter)) {
+
+			xmmsv_list_iter_entry (iter, &val);
+			xmmsv_get_coll (val, &op);
+
 			match = xmms_collection_media_match (dag, mediainfo, op,
 			                                     nsid, match_table);
-			xmmsv_coll_operand_list_next (coll);
 		}
-		xmmsv_coll_operand_list_restore (coll);
+		xmmsv_list_iter_explicit_destroy (iter);
 		break;
 
 	case XMMS_COLLECTION_TYPE_COMPLEMENT:
@@ -1924,14 +1946,14 @@ xmms_collection_media_match_operand (xmms_coll_dag_t *dag, GHashTable *mediainfo
                                      GHashTable *match_table)
 {
 	xmmsv_coll_t *op;
+	xmmsv_t *tmp;
 	gboolean match = FALSE;
 
-	xmmsv_coll_operand_list_save (coll);
-	xmmsv_coll_operand_list_first (coll);
-	if (xmmsv_coll_operand_list_entry (coll, &op)) {
+	if (xmmsv_list_get (xmmsv_coll_operands_get (coll), 0, &tmp)) {
+		xmmsv_get_coll (tmp, &op);
+
 		match = xmms_collection_media_match (dag, mediainfo, op, nsid, match_table);
 	}
-	xmmsv_coll_operand_list_restore (coll);
 
 	return match;
 }

src/xmms/collserial.c
@@ -260,18 +260,26 @@ xmms_collection_dbwrite_operator (xmms_medialib_session_t *session,
 	/* Save operands and connections (don't recurse in ref operand) */
 	newid = collid + 1;
 	if (xmmsv_coll_get_type (coll) != XMMS_COLLECTION_TYPE_REFERENCE) {
-		xmmsv_coll_operand_list_save (coll);
-		xmmsv_coll_operand_list_first (coll);
-		while (xmmsv_coll_operand_list_entry (coll, &op)) {
+		xmmsv_t *tmp;
+		xmmsv_list_iter_t *iter;
+
+		xmmsv_get_list_iter (xmmsv_coll_operands_get (coll), &iter);
+
+		for (xmmsv_list_iter_first (iter);
+		     xmmsv_list_iter_valid (iter);
+		     xmmsv_list_iter_next (iter)) {
+
+			xmmsv_list_iter_entry (iter, &tmp);
+			xmmsv_get_coll (tmp, &op);
+
 			nextid = xmms_collection_dbwrite_operator (session, newid, op);
 			g_snprintf (query, sizeof (query),
 			            "INSERT INTO CollectionConnections VALUES(%d, %d)",
 			            newid, collid);
 			xmms_medialib_select (session, query, NULL);
 			newid = nextid;
-			xmmsv_coll_operand_list_next (coll);
 		}
-		xmmsv_coll_operand_list_restore (coll);
+		xmmsv_list_iter_explicit_destroy (iter);
 	}
 
 	/* return next available id */

src/xmms/playlist.c
@@ -240,6 +240,7 @@ xmms_playlist_update_partyshuffle (xmms_playlist_t *playlist,
 {
 	gint history, upcoming, currpos, size;
 	xmmsv_coll_t *src;
+	xmmsv_t *tmp;
 
 	XMMS_DBG ("PLAYLIST: update-partyshuffle!");
 
@@ -258,13 +259,11 @@ xmms_playlist_update_partyshuffle (xmms_playlist_t *playlist,
 		currpos = xmms_playlist_coll_get_currpos (coll);
 	}
 
-	xmmsv_coll_operand_list_save (coll);
-	xmmsv_coll_operand_list_first (coll);
-	if (!xmmsv_coll_operand_list_entry (coll, &src)) {
+	if (!xmmsv_list_get (xmmsv_coll_operands_get (coll), 0, &tmp)) {
 		XMMS_DBG ("Cannot find party shuffle operand!");
 		return;
 	}
-	xmmsv_coll_operand_list_restore (coll);
+	xmmsv_get_coll (tmp, &src);
 
 	currpos = xmms_playlist_coll_get_currpos (coll);
 	size = xmms_playlist_coll_get_size (coll);

tests/xmmsv/t_coll.c
@@ -130,7 +130,7 @@ CASE (test_coll_operands_list_clear)
 	u = xmmsv_coll_new (XMMS_COLLECTION_TYPE_UNION);
 	CU_ASSERT_PTR_NOT_NULL (u);
 
-	xmmsv_coll_operand_list_clear (u);
+	xmmsv_list_clear (xmmsv_coll_operands_get (u));
 
 	xmmsv_coll_unref (u);
 }
@@ -159,7 +159,7 @@ CASE (test_coll_operands)
 		xmmsv_coll_unref (ops[i]);
 	}
 
-	xmmsv_coll_operand_list_clear (u);
+	xmmsv_list_clear (xmmsv_coll_operands_get (u));
 
 	xmmsv_coll_unref (u);
 }
@@ -194,7 +194,7 @@ CASE (test_coll_operands_list)
 
 	CU_ASSERT_EQUAL (xmmsv_list_get_size (xmmsv_coll_operands_get (u)), 10);
 
-	xmmsv_coll_operand_list_clear (u);
+	xmmsv_list_clear (xmmsv_coll_operands_get (u));
 
 	xmmsv_coll_unref (u);
 
@@ -207,6 +207,7 @@ CASE (test_coll_attributes)
 	xmmsv_coll_t *c;
 	int cnt;
 	int sum;
+	xmmsv_dict_iter_t *it;
 
 	c = xmmsv_coll_new (XMMS_COLLECTION_TYPE_REFERENCE);
 	CU_ASSERT_PTR_NOT_NULL (c);
@@ -227,17 +228,17 @@ CASE (test_coll_attributes)
 
 	cnt = 0;
 	sum = 0;
-	xmmsv_coll_attribute_list_first (c);
-	while (xmmsv_coll_attribute_list_valid (c)) {
+	CU_ASSERT_TRUE (xmmsv_get_dict_iter (xmmsv_coll_attributes_get (c), &it));
+	for (xmmsv_dict_iter_first (it);
+	     xmmsv_dict_iter_valid (it);
+	     xmmsv_dict_iter_next (it)) {
 		const char *k;
 		const char *v;
 
-		xmmsv_coll_attribute_list_entry (c, &k, &v);
+		CU_ASSERT_TRUE (xmmsv_dict_iter_pair_string (it, &k, &v));
 		CU_ASSERT_EQUAL (k[1], v[1]);
 		sum += atoi (k + 1);
 		cnt++;
-
-		xmmsv_coll_attribute_list_next (c);
 	}
 	CU_ASSERT_EQUAL (cnt, 3);
 	CU_ASSERT_EQUAL (sum, 1+2+3);

commit 360eaf283b3553c3f096ca61474e430edc6b05f7

BUG(2241): Fix resolving REFERENCEs
where the collection referred to is not bound as an operand.
(Bug introduced in c0c276599ac470c9136653736a8b3fe7b45ee462)

src/xmms/collquery.c
@@ -415,10 +415,8 @@ query_append_operand (coll_query_t *query, xmms_coll_dag_t *dag, xmmsv_coll_t *c
 	gchar *target_name;
 	gchar *target_ns;
 	guint  target_nsid;
-	xmmsv_t *tmp;
 
-	if (xmmsv_list_get (xmmsv_coll_operands_get (coll), 0, &tmp)) {
-		xmmsv_get_coll (tmp, &op);
+	if (!xmmsv_list_get_coll (xmmsv_coll_operands_get (coll), 0, &op)) {
 
 		/* Ref'd coll not saved as operand, look for it */
 		if (xmmsv_coll_attribute_get (coll, "reference", &target_name) &&