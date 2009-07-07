3d13525fd0d97d91724d4e2ee4be24e7e8d6cf9d
Anders Waldenborg <andersg@xmms.org>
2009-07-07 08:55:42 +0200

    BUG(2243): Start mediainfo reader _after_ initializing colldag.

--- src/xmms/playlist.c
+++ src/xmms/playlist.c
@@ -404,8 +404,8 @@ xmms_playlist_init (void)
 	                     XMMS_CMD_FUNC (rinsert));
 
 	ret->medialib = xmms_medialib_init (ret);
-	ret->mediainfordr = xmms_mediainfo_reader_start ();
 	ret->colldag = xmms_collection_init (ret);
+	ret->mediainfordr = xmms_mediainfo_reader_start ();
 
 	return ret;
 }
