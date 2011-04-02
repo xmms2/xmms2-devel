b01835475c1b998aa727a433e78356cfe24f695f
Erik Massop <e.massop@hccnet.nl>
2011-04-02 02:12:05 +0200

    OTHER: Let (DICT|LIST)_ENTRY_(STR|COLL) accept NULL, returning XMMSV_TYPE_NONE.
    Formerly (DICT|LIST)_ENTRY_(STR|COLL) (NULL) would return NULL, and the list or
    dict would be prematurely terminated.

--- src/include/xmmsc/xmmsv.h
+++ src/include/xmmsc/xmmsv.h
@@ -196,16 +196,17 @@ int xmmsv_utf8_validate (const char *str);
 /* These helps us doing compiletime typechecking */
 static inline const char *__xmmsv_identity_const_charp (const char *v) {return v;}
 static inline xmmsv_t *__xmmsv_identity_xmmsv (xmmsv_t *v) {return v;}
+static inline xmmsv_t *__xmmsv_null_to_none (xmmsv_t *v) { return v ? v : xmmsv_new_none (); }
 #define XMMSV_DICT_ENTRY(k, v) __xmmsv_identity_const_charp (k), __xmmsv_identity_xmmsv (v)
-#define XMMSV_DICT_ENTRY_STR(k, v) XMMSV_DICT_ENTRY (k, xmmsv_new_string (v))
+#define XMMSV_DICT_ENTRY_STR(k, v) XMMSV_DICT_ENTRY (k, __xmmsv_null_to_none (xmmsv_new_string (v)))
 #define XMMSV_DICT_ENTRY_INT(k, v) XMMSV_DICT_ENTRY (k, xmmsv_new_int (v))
 #define XMMSV_DICT_END NULL
 xmmsv_t *xmmsv_build_dict (const char *firstkey, ...);
 
 #define XMMSV_LIST_ENTRY(v) __xmmsv_identity_xmmsv (v)
-#define XMMSV_LIST_ENTRY_STR(v) XMMSV_LIST_ENTRY (xmmsv_new_string (v))
+#define XMMSV_LIST_ENTRY_STR(v) XMMSV_LIST_ENTRY (__xmmsv_null_to_none (xmmsv_new_string (v)))
 #define XMMSV_LIST_ENTRY_INT(v) XMMSV_LIST_ENTRY (xmmsv_new_int (v))
-#define XMMSV_LIST_ENTRY_COLL(v) XMMSV_LIST_ENTRY (xmmsv_new_coll (v))
+#define XMMSV_LIST_ENTRY_COLL(v) XMMSV_LIST_ENTRY (__xmmsv_null_to_none (xmmsv_new_coll (v)))
 #define XMMSV_LIST_END NULL
 
 xmmsv_t *xmmsv_build_list (xmmsv_t *first_entry, ...);
