bec44e4cba119cef04e6754ad727bf45ef3a199f
Daniel Svensson <dsvensson@gmail.com>
2011-08-04 03:15:55 +0200

    OTHER: Add some signal checks to playlist test.

--- tests/server/t_playlist.c
+++ tests/server/t_playlist.c
@@ -10,6 +10,7 @@
 #include "utils/jsonism.h"
 #include "utils/value_utils.h"
 #include "utils/coll_utils.h"
+#include "utils/ipc_call.h"
 
 static xmms_medialib_t *medialib;
 static xmms_coll_dag_t *colldag;
@@ -66,7 +67,9 @@ CASE (test_basic_functionality)
 {
 	xmms_medialib_session_t *session;
 	xmms_error_t err;
+	xmms_future_t *future;
 	gint first, second, result;
+	xmmsv_t *signals, *expected;
 
 	session = xmms_medialib_session_begin (medialib);
 
@@ -77,21 +80,54 @@ CASE (test_basic_functionality)
 
 	xmms_medialib_session_commit (session);
 
+	future = XMMS_IPC_CHECK_SIGNAL (playlist, XMMS_IPC_SIGNAL_PLAYLIST_CHANGED);
+
+	/* emits CHANGED_UPDATE, and CHANGED_ADD */
 	xmms_playlist_add_entry (playlist, XMMS_ACTIVE_PLAYLIST, first, &err);
+	/* emits CHANGED_UPDATE, and CHANGED_ADD */
 	xmms_playlist_add_entry (playlist, XMMS_ACTIVE_PLAYLIST, second, &err);
 
+	signals = xmms_future_await_many (future, 4);
+
+	/* XMMS_PLAYLIST_CHANGED_ADD = 0, XMMS_PLAYLIST_CHANGED_UPDATE = 7 */
+	expected = xmmsv_from_json ("[{                         'type': 7, 'name': 'Default' },"
+	                            " { 'position': 0, 'id': 2, 'type': 0, 'name': 'Default' },"
+	                            " {                         'type': 7, 'name': 'Default' },"
+	                            " { 'position': 1, 'id': 3, 'type': 0, 'name': 'Default' }]");
+
+	CU_ASSERT (xmmsv_compare (expected, signals));
+	xmmsv_unref (signals);
+	xmmsv_unref (expected);
+	xmms_future_free (future);
+
+	future = XMMS_IPC_CHECK_SIGNAL (playlist, XMMS_IPC_SIGNAL_PLAYLIST_CURRENT_POS);
+
+	/* emit CURRPOS_MSG pointing at first position as last position was -1 */
 	result = xmms_playlist_current_entry (playlist);
 	CU_ASSERT_EQUAL (first, result);
 
+	/* emit CURRPOS_MSG -> moves to second entry */
 	result = xmms_playlist_advance (playlist);
 	CU_ASSERT_EQUAL (TRUE, result);
 
 	result = xmms_playlist_current_entry (playlist);
 	CU_ASSERT_EQUAL (second, result);
 
+	/* emit CURRPOS_MSG -> hits end of list, back to first position */
 	result = xmms_playlist_advance (playlist);
 	CU_ASSERT_EQUAL (FALSE, result);
 
 	result = xmms_playlist_current_entry (playlist);
 	CU_ASSERT_EQUAL (first, result);
+
+	signals = xmms_future_await_many (future, 3);
+
+	expected = xmmsv_from_json ("[{ 'position': 0, 'name': 'Default' },"
+	                            " { 'position': 1, 'name': 'Default' },"
+	                            " { 'position': 0, 'name': 'Default' }]");
+
+	CU_ASSERT (xmmsv_compare (expected, signals));
+	xmmsv_unref (signals);
+	xmmsv_unref (expected);
+	xmms_future_free (future);
 }
