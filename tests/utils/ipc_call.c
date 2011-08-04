0827f220d66da0a7abc53d3235479548cf663b19
Daniel Svensson <dsvensson@gmail.com>
2011-08-04 03:14:58 +0200

    OTHER: Add tools to verify object signals in tests.

--- tests/utils/ipc_call.c
+++ tests/utils/ipc_call.c
@@ -14,7 +14,18 @@
  *  Lesser General Public License for more details.
  */
 
+#include <stdio.h>
+
 #include "ipc_call.h"
+#include "xmmsc/xmmsc_util.h"
+
+struct xmms_future_St {
+	xmmsv_t *result;
+	xmms_object_t *object;
+	gint message;
+	gint delay;
+	gint timeout;
+};
 
 xmmsv_t *
 __xmms_ipc_call (xmms_object_t *object, gint cmd, ...)
@@ -47,4 +58,111 @@ __xmms_ipc_call (xmms_object_t *object, gint cmd, ...)
 	}
 
 	return xmmsv_new_error (arg.error.message);
+}
+
+static void
+future_callback (xmms_object_t *object, xmmsv_t *val, gpointer udata)
+{
+	xmms_future_t *future = (xmms_future_t *) udata;
+
+	xmmsv_list_append (future->result, val);
+}
+
+xmms_future_t *
+__xmms_ipc_check_signal (xmms_object_t *object, gint message,
+                         gint delay, gint timeout)
+{
+	xmms_future_t *future = g_new0 (xmms_future_t, 1);
+
+	xmms_object_ref (object);
+
+	future->result = xmmsv_new_list ();
+	future->object = object;
+	future->message = message;
+
+	future->delay = delay;
+	future->timeout = timeout;
+
+	xmms_object_connect (future->object, future->message,
+	                     future_callback, future);
+
+	return future;
+}
+
+void
+xmms_future_free (xmms_future_t *future)
+{
+	xmms_object_disconnect (future->object, future->message,
+	                        future_callback, future);
+	xmms_object_unref (future->object);
+	xmmsv_unref (future->result);
+	g_free (future);
+}
+
+/**
+ * Fetch one signal result.
+ *
+ * Waits for one entry to enter the result list and then clears
+ * the list and return that entry.
+ *
+ * TODO: Should check result, wait X ms, try again, repeat all
+ *       until Y ms has passed.
+ *
+ * @return the first signal in queue, or NULL on error, ownership transferred.
+ */
+xmmsv_t *
+xmms_future_await_one (xmms_future_t *future)
+{
+	xmmsv_t *entry;
+
+	if (future->result == NULL) {
+		return NULL;
+	}
+
+	if (!xmmsv_list_get (future->result, 0, &entry)) {
+		fprintf (stderr, "Expected one element, found none\n");
+		xmms_dump_stack ();
+		return NULL;
+	}
+
+	xmmsv_ref (entry);
+
+	xmmsv_list_remove (future->result, 0);
+
+	return entry;
+}
+
+/**
+ * Fetch one signal result.
+ *
+ * Waits until a certain number of signal results have
+ * been cought, then creates a new result list and
+ * returns the signals cought.
+ *
+ * TODO: Should check result, wait X ms, try again, repeat all
+ *       until Y ms has passed.
+ *
+ * @return all signals in queue, or NULL on error, ownership transferred.
+ */
+xmmsv_t *
+xmms_future_await_many (xmms_future_t *future, gint count)
+{
+	xmmsv_t *result;
+
+	if (future->result == NULL) {
+		return NULL;
+	}
+
+	if (xmmsv_list_get_size (future->result) != count) {
+		fprintf (stderr, "Expected size %d != %d\n",
+		         count, xmmsv_list_get_size (future->result));
+		xmms_dump_stack ();
+		return NULL;
+	}
+
+	result = future->result;
+
+	future->result = xmmsv_new_list ();
+
+	return result;
 }
--- tests/utils/ipc_call.h
+++ tests/utils/ipc_call.h
@@ -25,4 +25,16 @@
 xmmsv_t *__xmms_ipc_call (xmms_object_t *object, gint cmd, ...);
 #define XMMS_IPC_CALL(obj, cmd, ...) __xmms_ipc_call (XMMS_OBJECT (obj), cmd, __VA_ARGS__, NULL);
 
+typedef struct xmms_future_St xmms_future_t;
+
+xmms_future_t *__xmms_ipc_check_signal (xmms_object_t *object, gint message, gint delay, gint timeout);
+
+#define XMMS_FUTURE_DELAY_DEFAULT 75
+#define XMMS_FUTURE_TIMEOUT_DEFAULT 2500
+#define XMMS_IPC_CHECK_SIGNAL(obj, cmd) __xmms_ipc_check_signal (XMMS_OBJECT (obj), cmd, XMMS_FUTURE_DELAY_DEFAULT, XMMS_FUTURE_TIMEOUT_DEFAULT);
+
+void xmms_future_free (xmms_future_t *future);
+xmmsv_t *xmms_future_await_one (xmms_future_t *future);
+xmmsv_t *xmms_future_await_many (xmms_future_t *future, gint count);
+
 #endif
