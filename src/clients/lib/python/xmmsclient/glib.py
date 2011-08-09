823b64182fdf332e39bd7b177fae592e95933ab7
Raphaël Bois <virtualdust@gmail.com>
2011-08-09 10:22:48 +0200

    OTHER: Fix import issue in python bindings

--- src/clients/lib/python/xmmsclient/glib.py
+++ src/clients/lib/python/xmmsclient/glib.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 """
 
  A GLib connector for PyGTK - 
@@ -9,7 +10,7 @@
 
 """
 
-import glib
+from gobject import io_add_watch, IO_OUT, IO_IN, source_remove
 
 class GLibConnector:
 	def __init__(self, xmms):
@@ -19,16 +20,16 @@ class GLibConnector:
 
 	def need_out(self, i):
 		if self.xmms.want_ioout() and self.out_id is None:
-			self.out_id = glib.io_add_watch(self.xmms.get_fd(), glib.IO_OUT, self.handle_out)
+			self.out_id = io_add_watch(self.xmms.get_fd(), IO_OUT, self.handle_out)
 
 	def handle_in(self, source, cond):
-		if cond == glib.IO_IN:
+		if cond == IO_IN:
 			return self.xmms.ioin()
 
 		return True
 
 	def handle_out(self, source, cond):
-		if cond == glib.IO_OUT:
+		if cond == IO_OUT and self.xmms.want_ioout():
 			self.xmms.ioout()
 		if not self.xmms.want_ioout():
 			self.out_id = None
@@ -39,14 +40,14 @@ class GLibConnector:
 		self.disconnect()
 		if not xmms is None:
 			self.xmms = xmms
-		xmms.set_need_out_fun(self.need_out)
-		self.in_id = glib.io_add_watch(self.xmms.get_fd(), glib.IO_IN, self.handle_in)
+		self.xmms.set_need_out_fun(self.need_out)
+		self.in_id = io_add_watch(self.xmms.get_fd(), IO_IN, self.handle_in)
 		self.out_id = None
 
 	def disconnect(self):
 		if not self.in_id is None:
-			glib.source_remove(self.in_id)
+			source_remove(self.in_id)
 			self.in_id = None
 		if not self.out_id is None:
-			glib.source_remove(self.out_id)
+			source_remove(self.out_id)
 			self.out_id = None
