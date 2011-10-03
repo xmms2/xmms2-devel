89153777296e3e1bda42b1b2f669e41704a3bc2b
Daniel Svensson <dsvensson@gmail.com>
2011-10-03 23:43:44 +0200

    BUG(2464): Backport subprocess exception.

--- waftools/cython_extra.py
+++ waftools/cython_extra.py
@@ -3,7 +3,21 @@
 
 import re
 from waflib import Configure
-from subprocess import Popen, STDOUT, PIPE, CalledProcessError
+from subprocess import Popen, STDOUT, PIPE
+
+# Borrowed from Python 2.7, subprocess.py
+class CalledProcessError(Exception):
+    """This exception is raised when a process run by check_call() or
+    check_output() returns a non-zero exit status.
+    The exit status will be stored in the returncode attribute;
+    check_output() will also store the output in the output attribute.
+    """
+    def __init__(self, returncode, cmd, output=None):
+        self.returncode = returncode
+        self.cmd = cmd
+        self.output = output
+    def __str__(self):
+        return "Command '%s' returned non-zero exit status %d" % (self.cmd, self.returncode)
 
 # Borrowed from Python 2.7, subprocess.py
 def check_output(*popenargs, **kwargs):
