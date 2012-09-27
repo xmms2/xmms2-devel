4a65078730b1c87ef1e8f6fe83bb198ba30f9c69
Daniel Svensson <dsvensson@gmail.com>
2012-09-28 01:33:56 +0200

    OTHER: Lazy-create medialib session instance members.

--- src/xmms/medialib_session.c
+++ src/xmms/medialib_session.c
@@ -30,6 +30,8 @@ struct xmms_medialib_session_St {
 static void xmms_medialib_session_free (xmms_medialib_session_t *session);
 static void xmms_medialib_session_free_full (xmms_medialib_session_t *session);
 
+static GHashTable *xmms_medialib_session_get_table (GHashTable **table);
+
 static void xmms_medialib_entry_send_added (xmms_medialib_t *medialib, xmms_medialib_entry_t entry);
 static void xmms_medialib_entry_send_update (xmms_medialib_t *medialib, xmms_medialib_entry_t entry);
 static void xmms_medialib_entry_send_removed (xmms_medialib_t *medialib, xmms_medialib_entry_t entry);
@@ -45,12 +47,6 @@ xmms_medialib_session_begin_internal (xmms_medialib_t *medialib,
 
 	s4_t *s4 = xmms_medialib_get_database_backend (medialib);
 	ret->trans = s4_begin (s4, flags);
-
-	ret->added = g_hash_table_new (NULL, NULL);
-	ret->updated = g_hash_table_new (NULL, NULL);
-	ret->removed = g_hash_table_new (NULL, NULL);
-
-	ret->vals = xmmsv_new_list ();
 
 	return ret;
 }
@@ -85,27 +81,36 @@ xmms_medialib_session_commit (xmms_medialib_session_t *session)
 		return FALSE;
 	}
 
-	g_hash_table_iter_init (&iter, session->added);
+	if (session->added != NULL) {
+		g_hash_table_iter_init (&iter, session->added);
 
-	while (g_hash_table_iter_next (&iter, &key, NULL)) {
-		xmms_medialib_entry_send_added (session->medialib,
-		                                GPOINTER_TO_INT (key));
-		g_hash_table_remove (session->updated, key);
+		while (g_hash_table_iter_next (&iter, &key, NULL)) {
+			xmms_medialib_entry_send_added (session->medialib,
+			                                GPOINTER_TO_INT (key));
+			if (session->updated != NULL)
+				g_hash_table_remove (session->updated, key);
+		}
 	}
 
-	g_hash_table_iter_init (&iter, session->removed);
+	if (session->removed != NULL) {
+		g_hash_table_iter_init (&iter, session->removed);
 
-	while (g_hash_table_iter_next (&iter, &key, NULL)) {
-		xmms_medialib_entry_send_removed (session->medialib,
-		                                  GPOINTER_TO_INT (key));
-		g_hash_table_remove (session->updated, key);
+		while (g_hash_table_iter_next (&iter, &key, NULL)) {
+			xmms_medialib_entry_send_removed (session->medialib,
+			                                  GPOINTER_TO_INT (key));
+			if (session->updated != NULL)
+				g_hash_table_remove (session->updated, key);
+		}
 	}
 
-	g_hash_table_iter_init (&iter, session->updated);
 
-	while (g_hash_table_iter_next (&iter, &key, NULL)) {
-		xmms_medialib_entry_send_update (session->medialib,
-		                                 GPOINTER_TO_INT (key));
+	if (session->updated != NULL) {
+		g_hash_table_iter_init (&iter, session->updated);
+
+		while (g_hash_table_iter_next (&iter, &key, NULL)) {
+			xmms_medialib_entry_send_update (session->medialib,
+			                                 GPOINTER_TO_INT (key));
+		}
 	}
 
 	xmms_medialib_session_free (session);
@@ -174,9 +179,9 @@ xmms_medialib_session_property_set (xmms_medialib_session_t *session,
 	s4_val_free (song_id);
 
 	if (strcmp (key, XMMS_MEDIALIB_ENTRY_PROPERTY_URL) == 0) {
-		events = session->added;
+		events = xmms_medialib_session_get_table (&session->added);
 	} else {
-		events = session->updated;
+		events = xmms_medialib_session_get_table (&session->updated);
 	}
 
 	g_hash_table_insert (events,
@@ -203,9 +208,9 @@ xmms_medialib_session_property_unset (xmms_medialib_session_t *session,
 	s4_val_free (song_id);
 
 	if (strcmp (key, XMMS_MEDIALIB_ENTRY_PROPERTY_URL) == 0) {
-		events = session->removed;
+		events = xmms_medialib_session_get_table (&session->removed);
 	} else {
-		events = session->updated;
+		events = xmms_medialib_session_get_table (&session->updated);
 	}
 
 	g_hash_table_insert (events,
@@ -219,9 +224,10 @@ void
 xmms_medialib_session_track_garbage (xmms_medialib_session_t *session,
                                      xmmsv_t *data)
 {
+	if (session->vals == NULL)
+		session->vals = xmmsv_new_list ();
 	xmmsv_list_append (session->vals, data);
 }
-
 
 static void
 xmms_medialib_session_free_full (xmms_medialib_session_t *session)
@@ -229,8 +235,10 @@ xmms_medialib_session_free_full (xmms_medialib_session_t *session)
 	xmmsv_t *val;
 	gint i;
 
-	for (i = 0; xmmsv_list_get (session->vals, i, &val); i++)
-		xmmsv_unref (val);
+	if (session->vals != NULL) {
+		for (i = 0; xmmsv_list_get (session->vals, i, &val); i++)
+			xmmsv_unref (val);
+	}
 
 	xmms_medialib_session_free (session);
 }
@@ -240,13 +248,24 @@ xmms_medialib_session_free (xmms_medialib_session_t *session)
 {
 	xmms_object_unref (session->medialib);
 
-	g_hash_table_unref (session->added);
-	g_hash_table_unref (session->updated);
-	g_hash_table_unref (session->removed);
-
-	xmmsv_unref (session->vals);
+	if (session->added != NULL)
+		g_hash_table_unref (session->added);
+	if (session->updated != NULL)
+		g_hash_table_unref (session->updated);
+	if (session->removed != NULL)
+		g_hash_table_unref (session->removed);
+	if (session->vals != NULL)
+		xmmsv_unref (session->vals);
 
 	g_free (session);
+}
+
+static GHashTable *
+xmms_medialib_session_get_table (GHashTable **table)
+{
+	if (*table == NULL)
+		*table = g_hash_table_new (g_direct_hash, g_direct_equal);
+	return *table;
 }
 
 /**
@@ -292,4 +311,3 @@ xmms_medialib_entry_send_removed (xmms_medialib_t *medialib, xmms_medialib_entry
 	                  XMMS_IPC_SIGNAL_MEDIALIB_ENTRY_REMOVED,
 	                  xmmsv_new_int (entry));
 }
-
