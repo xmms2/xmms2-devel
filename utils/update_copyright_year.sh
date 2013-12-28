f80d8befea6540bb69c51b96856f69ca750a2231
Erik Massop <e.massop@hccnet.nl>
2013-12-28 01:11:42 +0100

    OTHER: minor changes to update_copyright_year.sh
    Use -l to grep instead of cut.
    Accept a single year in the original notice.
    Allow only digits in second year.

--- utils/update_copyright_year.sh
+++ utils/update_copyright_year.sh
@@ -1,1 +1,2 @@
-git grep -E "Copyright.+XMMS2 Team" | cut -d ":" -f 1 | xargs -I {} sed -i -r -e"s/Copyright [^0-9]+([0-9]{4})-.+ XMMS2 Team/Copyright (C) \1-`date +%Y` XMMS2 Team/g" {}
+git grep -l -E "Copyright.+XMMS2 Team" | \
+    xargs -I {} sed -i -r -e"s/Copyright \(C\) ([0-9]{4})-?[0-9]* XMMS2 Team/Copyright (C) \1-`date +%Y` XMMS2 Team/g" {}
