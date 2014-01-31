xmms2/xmms2-alumni

OTHER: Make the hello server command return the client's id.
The client's id is now stored in the connection structure.

src/includepriv/xmmsclientpriv/xmmsclient.h
@@ -56,6 +56,9 @@ struct xmmsc_connection_St {
 	int visc;
 	xmmsc_visualization_t **visv;
 
+	/* this client's id, assigned by the server */
+	int32_t id;
+
 	/* we need to hold the connection path to get the hostname */
 	char path[XMMS_PATH_MAX];
 };

src/xmms/ipc.c
@@ -78,7 +78,12 @@ typedef struct xmms_ipc_client_St {
 
 	guint pendingsignals[XMMS_IPC_SIGNAL_END];
 	GList *broadcasts[XMMS_IPC_SIGNAL_END];
+
+	gint32 id;
 } xmms_ipc_client_t;
+
+/* id 0 is reserved for the server */
+static gint32 next_client_id = 1;
 
 static GMutex *ipc_servers_lock;
 static GList *ipc_servers = NULL;
@@ -397,6 +402,7 @@ xmms_ipc_client_new (xmms_ipc_t *ipc, xmms_ipc_transport_t *transport)
 	client->ipc = ipc;
 	client->out_msg = g_queue_new ();
 	client->lock = g_mutex_new ();
+	client->id = next_client_id++;
 
 	return client;
 }

xmms2/xmms2-devel

src/includepriv/xmmsclientpriv/xmmsclient.h
@@ -57,6 +57,9 @@ struct xmmsc_connection_St {
 	int visc;
 	xmmsc_visualization_t **visv;
 
+	/* this client's id, assigned by the server */
+	int64_t id;
+
 	/* we need to hold the connection path to get the hostname */
 	char path[XMMS_PATH_MAX];
 };

src/xmms/ipc.c
@@ -77,7 +77,12 @@ typedef struct xmms_ipc_client_St {
 
 	guint pendingsignals[XMMS_IPC_SIGNAL_END];
 	GList *broadcasts[XMMS_IPC_SIGNAL_END];
+
+	gint64 id;
 } xmms_ipc_client_t;
+
+/* id 0 is reserved for the server */
+static gint64 next_client_id = 1;
 
 static GMutex ipc_servers_lock;
 static GList *ipc_servers = NULL;
@@ -395,6 +400,7 @@ xmms_ipc_client_new (xmms_ipc_t *ipc, xmms_ipc_transport_t *transport)
 	client->ipc = ipc;
 	client->out_msg = g_queue_new ();
 	g_mutex_init (&client->lock);
+	client->id = next_client_id++;
 
 	return client;
 }