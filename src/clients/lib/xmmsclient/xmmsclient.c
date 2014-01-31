xmms2/xmms2-alumni

FEATURE(1598): Basic c2c in libxmmsclient
Introduce new notifier types for c2c messages that allow clients to
handle results independently from their origin (service client or
server):

  - 'default' callbacks receive the value as sent by the server or another
    client, that is, for c2c messages only the payload is passed to the
    callback.

  - 'raw' callbacks receive the full c2c message when associated with
    messages from other clients, and the regular value as sent by the
    server otherwise.

  - 'c2c' callbacks always receive values formatted as c2c messages,
    regardless of whether the sender is the server or a different client.
    For messages sent by the server, the sender id and message id fields
    will be zero.

src/clients/lib/xmmsclient/xmmsclient.c
@@ -37,6 +37,7 @@
 #define XMMS_MAX_URI_LEN 1024
 
 static void xmmsc_deinit (xmmsc_connection_t *c);
+static xmms_ipc_msg_t *xmmsc_build_ipc_msg_va (int obj, int cmd, xmmsv_t *first_arg, va_list ap);
 
 /*
  * Public methods
@@ -433,20 +434,43 @@ xmmsc_result_t *
 xmmsc_send_cmd (xmmsc_connection_t *c, int obj, int cmd,
                 xmmsv_t *first_arg, ...)
 {
-	xmms_ipc_msg_t *msg;
-	xmmsv_t *args;
 	va_list ap;
-
-	msg = xmms_ipc_msg_new (obj, cmd);
+	xmms_ipc_msg_t *msg;
 
 	va_start (ap, first_arg);
-	args = xmmsv_build_list_va (first_arg, ap);
+	msg = xmmsc_build_ipc_msg_va (obj, cmd, first_arg, ap);
 	va_end (ap);
+
+	return xmmsc_send_msg (c, msg);
+}
+
+uint32_t
+xmmsc_send_cmd_cookie (xmmsc_connection_t *c, int obj, int cmd,
+                       xmmsv_t *first_arg, ...)
+{
+	va_list ap;
+	xmms_ipc_msg_t *msg;
+
+	va_start (ap, first_arg);
+	msg = xmmsc_build_ipc_msg_va (obj, cmd, first_arg, ap);
+	va_end (ap);
+
+	return xmmsc_write_msg_to_ipc (c, msg);
+}
+
+static xmms_ipc_msg_t *
+xmmsc_build_ipc_msg_va (int obj, int cmd, xmmsv_t *first_arg, va_list ap)
+{
+	xmmsv_t *args;
+	xmms_ipc_msg_t *msg;
+
+	msg = xmms_ipc_msg_new (obj, cmd);
+	args = xmmsv_build_list_va (first_arg, ap);
 
 	xmms_ipc_msg_put_value (msg, args);
 	xmmsv_unref (args);
 
-	return xmmsc_send_msg (c, msg);
+	return msg;
 }
 
 /**

xmms2/xmms2-devel

src/clients/lib/xmmsclient/xmmsclient.c
@@ -37,6 +37,7 @@
 #define XMMS_MAX_URI_LEN 1024
 
 static void xmmsc_deinit (xmmsc_connection_t *c);
+static xmms_ipc_msg_t *xmmsc_build_ipc_msg_va (int obj, int cmd, xmmsv_t *first_arg, va_list ap);
 
 /*
  * Public methods
@@ -448,6 +449,27 @@ xmmsc_send_cmd (xmmsc_connection_t *c, int obj, int cmd, ...)
 	xmmsv_unref (args);
 
 	return xmmsc_send_msg (c, msg);
+}
+
+uint32_t
+xmmsc_send_cmd_cookie (xmmsc_connection_t *c, int obj, int cmd, ...)
+{
+	xmmsv_t *first_arg;
+	xmms_ipc_msg_t *msg;
+	xmmsv_t *args;
+	va_list ap;
+
+	msg = xmms_ipc_msg_new (obj, cmd);
+
+	va_start (ap, cmd);
+	first_arg = va_arg (ap, xmmsv_t *);
+	args = xmmsv_build_list_va (first_arg, ap);
+	va_end (ap);
+
+	xmms_ipc_msg_put_value (msg, args);
+	xmmsv_unref (args);
+
+	return xmmsc_write_msg_to_ipc (c, msg);
 }
 
 /**