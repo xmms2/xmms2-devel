02fe74303ac7c63f91f6d7c9c16a02aaa4dc2be2
Daniel Svensson <dsvensson@gmail.com>
2015-04-11 01:06:17 +0200

    OTHER: Use LIBDIR instead of hardcoded PREFIX/lib.

--- waftools/tool.py
+++ waftools/tool.py
@@ -5,5 +5,5 @@ def add_install_flag(bld, obj):
 
     if env['explicit_install_name']:
         libname = obj.env["cshlib_PATTERN"] % obj.target
-        insname = os.path.join(obj.env["PREFIX"], 'lib', libname)
+        insname = os.path.join(obj.env["LIBDIR"], libname)
         obj.env.append_unique("LINKFLAGS", ["-install_name", insname])
