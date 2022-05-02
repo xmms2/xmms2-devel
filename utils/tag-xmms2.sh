d9b86a43182bb68fc089a589d3a04049c6e55121
Sergei Trofimovich <slyich@gmail.com>
2022-05-02 19:25:16 +0100

    utils/tag-xmms2.sh: turn the script into a dry-run

--- utils/tag-xmms2.sh
+++ utils/tag-xmms2.sh
@@ -5,14 +5,7 @@ export GIT_AUTHOR_NAME="XMMS2 Release"
 export GIT_AUTHOR_EMAIL="release@xmms2.org"
 
 echo -e "New version: '$VERSION' (tag: $TAG)\n"
-git diff
+git diff | cat
 
-echo -n "Accept? [y/N] "
-read ACCEPT
-
-if [ x$ACCEPT = "xY" ] || [ x$ACCEPT = "xy" ]; then
-	git commit -a -m "RELEASE: $VERSION"
-	git tag -u 7A8057EF -s -m "$VERSION" $TAG
-else
-	echo "Aborting..."
-fi
+echo DRY RUN: git commit -a -m "'RELEASE: $VERSION'"
+echo DRY RUN: git tag -u 7A8057EF -s -m "'$VERSION'" $TAG
